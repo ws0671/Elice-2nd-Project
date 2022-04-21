6746fcf09512128a34c4eee77a80b6f4aed5f126
SHIN DOHEE <DOHEE@DESKTOP-VHAC2D0.localdomain>
2022-04-21 16:30:29 +0900

    [Feat] register service 생성

--- back/src/services/userService.js
+++ back/src/services/userService.js
@@ -4,12 +4,19 @@ import { v4 as uuidv4 } from "uuid";
 import jwt from "jsonwebtoken";
 
 class userAuthService {
-  static async addUser({ name, email, password }) {
+  static async addUser({ nickname, email, password }) {
     // 이메일 중복 확인
-    const user = await User.findByEmail({ email });
-    if (user) {
+    const userEmail = await User.findByEmail({ email });
+    if (userEmail) {
       const errorMessage =
         "이 이메일은 현재 사용중입니다. 다른 이메일을 입력해 주세요.";
+      return { errorMessage };
+    }
+
+    const userNickname = await User.findByNickname({ nickname });
+    if (userNickname) {
+      const errorMessage =
+        "이 닉네임은 현재 사용중입니다. 다른 닉네임을 입력해 주세요.";
       return { errorMessage };
     }
 
@@ -18,7 +25,7 @@ class userAuthService {
 
     // id 는 유니크 값 부여
     const id = uuidv4();
-    const newUser = { id, name, email, password: hashedPassword };
+    const newUser = { id, nickname, email, password: hashedPassword };
 
     // db에 저장
     const createdNewUser = await User.create({ newUser });
@@ -80,8 +87,7 @@ class userAuthService {
 
     // db에서 찾지 못한 경우, 에러 메시지 반환
     if (!user) {
-      const errorMessage =
-        "가입 내역이 없습니다. 다시 한 번 확인해 주세요.";
+      const errorMessage = "가입 내역이 없습니다. 다시 한 번 확인해 주세요.";
       return { errorMessage };
     }
 
