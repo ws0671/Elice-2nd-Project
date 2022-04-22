62256a9bc7441becffd250898388fa32657726ee
SHIN DOHEE <DOHEE@DESKTOP-VHAC2D0.localdomain>
2022-04-22 15:08:13 +0900

    [Fix] 게임 리스트 조회 라우터와 개별 게임 조회 라우터 순서 변경

--- back/src/routers/gameRouter.js
+++ back/src/routers/gameRouter.js
@@ -5,6 +5,20 @@ import { gameAuthService } from "../services/gameService"
 
 const gameAuthRouter = Router()
 
+gameAuthRouter.get("/game/list", async function (req, res, next) {
+  try {
+    const gamelist = await gameAuthService.getGames()
+
+    if (gamelist.errorMessage) {
+      throw new Error(gamelist.errorMessage)
+    }
+
+    res.status(200).send(gamelist)
+  } catch (error) {
+    next(error)
+  }
+})
+
 gameAuthRouter.get("/game/:gameId", async function (req, res, next) {
   try {
     const gameId = req.params.gameId
@@ -17,20 +31,6 @@ gameAuthRouter.get("/game/:gameId", async function (req, res, next) {
     }
 
     res.status(200).send(gameInfo)
-  } catch (error) {
-    next(error)
-  }
-})
-
-gameAuthRouter.get("/game/list", async function (req, res, next) {
-  try {
-    const gamelist = await gameAuthService.getGames()
-
-    if (gamelist.errorMessage) {
-      throw new Error(gamelist.errorMessage)
-    }
-
-    res.status(200).send(gamelist)
   } catch (error) {
     next(error)
   }
