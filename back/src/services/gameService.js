4f235108c6aaf2fd411cb10b2e70dd7be6e80095
SHIN DOHEE <DOHEE@DESKTOP-VHAC2D0.localdomain>
2022-04-22 14:41:59 +0900

    [Feat] 개별 게임 정보 조회 service층 구현

--- back/src/services/gameService.js
+++ back/src/services/gameService.js
@@ -1,4 +1,14 @@
 import { Game } from "../db" // from을 폴더(db) 로 설정 시, 디폴트로 index.js 로부터 import함.
 import { v4 as uuidv4 } from "uuid"
 
-const gameAuthService = {}
+const gameAuthService = {
+  getGameInfo: async ({ gameId }) => {
+    const game = await Game.findById({ gameId })
+    if (!game) {
+      throw new Error("해당 게임이 없습니다. 다시 한 번 확인해 주세요.")
+    }
+    return game
+  },
+}
+
+export { gameAuthService }
