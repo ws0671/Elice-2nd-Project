4e9e31396849520aa257916dab84c68194cbe057
joohe71 <joohe71@gmail.com>
2022-04-22 16:50:10 +0900

    Update: 폼 구성요소 추가 및 기능 구현(ing)

--- front/src/components/user/RegisterForm.js
+++ front/src/components/user/RegisterForm.js
@@ -1,5 +1,193 @@
-const Registerform = () => {
-  return <div>회원가입 폼</div>
+import { useState, useEffect } from "react"
+import axios from "axios"
+import styles from "./Register.module.css"
+
+const RegisterForm = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [nickname, setNickname] = useState("")
+
+  const [nameError, setNameError] = useState(true)
+  const [emailError, setEmailError] = useState(true)
+  const [passwordError, setPasswordError] = useState(true)
+  const [confirmPasswordError, setConfirmPasswordError] = useState(true)
+  const [nicknameError, setNicknameError] = useState(true)
+
+  const validateName = (name) => {
+    if (name.length < 1 || name.length > 10) {
+      setNameError(true)
+    } else {
+      setNameError(false)
+    }
+  }
+
+  const validateEmail = (email) => {
+    const validate = email
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )
+
+    setEmailError(!validate)
+  }
+
+  const validatePassword = (password) => {
+    const specialLetter = password.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi)
+    const isValidPassword = password.length >= 4 && specialLetter >= 1
+    if (isValidPassword) {
+      setPasswordError(false)
+    } else {
+      setPasswordError(true)
+    }
+  }
+
+  const validateConfirmPassword = (v) => {
+    if (v === password) {
+      setConfirmPasswordError(false)
+    } else {
+      setConfirmPasswordError(true)
+    }
+  }
+
+  const handleNameChange = (e) => {
+    setName(e.target.value)
+    validateName(e.target.value)
+  }
+
+  const handleIdChange = (e) => {
+    setEmail(e.target.value)
+    validateEmail(e.target.value)
+  }
+
+  const handlePwChange = (e) => {
+    setPassword(e.target.value)
+    validatePassword(e.target.value)
+  }
+
+  const handleConfirmPwChange = (e) => {
+    setConfirmPassword(e.target.value)
+    validateConfirmPassword(e.target.value)
+  }
+
+  const handleReset = () => {
+    setName("")
+    setEmail("")
+    setPassword("")
+    setConfirmPassword("")
+    setNameError(true)
+    setEmailError(true)
+    setPasswordError(true)
+    setConfirmPasswordError(true)
+  }
+
+  const handleSubmit = () => {
+    if (!nameError && !emailError && !passwordError) {
+      alert("회원가입에 성공했습니다.")
+      const newUser = { name, email, password }
+      axios.post("http://localhost:8080/register", newUser).then((res) => {
+        console.log(res.data)
+        handleReset()
+      })
+    } else {
+      alert("회원가입에 실패했습니다. 다시 한 번 확인해주세요.")
+    }
+  }
+
+  return (
+    <>
+      <form className={styles.formContainer}>
+        <fieldset className={styles.formFieldset}>
+          <label className={styles.formLabel} htmlFor="name">
+            이름
+          </label>
+          <input
+            value={name}
+            id="name"
+            onChange={handleNameChange}
+            type="text"
+            name="name"
+            placeholder="이름을 입력해 주세요."
+          ></input>
+          <div className={styles.error}>
+            {nameError ? "이름은 1글자 이상, 10글자 이하여야 합니다." : null}
+          </div>
+        </fieldset>
+
+        <fieldset className={styles.formFieldset}>
+          <label className={styles.formLabel} htmlFor="identification">
+            아이디
+          </label>
+          <input
+            value={email}
+            id="identification"
+            onChange={handleIdChange}
+            type="text"
+            name="identification"
+            placeholder="아이디를 입력해 주세요."
+          ></input>
+          <div className={styles.error}>
+            {emailError ? "아이디는 4글자 이상이어야 합니다." : null}
+          </div>
+        </fieldset>
+
+        <fieldset className={styles.formFieldset}>
+          <label className={styles.formLabel} htmlFor="identification">
+            비밀번호
+          </label>
+          <input
+            value={password}
+            id="password"
+            onChange={handlePwChange}
+            type="password"
+            name="password"
+            placeholder="비밀번호를 입력해 주세요."
+          ></input>
+          <div className={styles.error}>
+            {passwordError
+              ? "비밀번호는 특수문자 1개를 포함하여 4글자 이상이어야 합니다."
+              : null}
+          </div>
+        </fieldset>
+
+        <fieldset className={styles.formFieldset}>
+          <label className={styles.formLabel} htmlFor="identification">
+            비밀번호 확인
+          </label>
+          <input
+            value={confirmPassword}
+            id="confirmpassword"
+            onChange={handleConfirmPwChange}
+            type="password"
+            name="password"
+            placeholder="비밀번호 확인을 위해 다시 한 번 입력해 주세요."
+          ></input>
+          <div className={styles.error}>
+            {confirmPasswordError ? "비밀번호가 일치하지 않습니다" : null}
+          </div>
+        </fieldset>
+
+        <div className={styles.buttonContainer}>
+          <button
+            onClick={handleReset}
+            type="button"
+            className={`${styles.formButton} ${styles.resetButton}`}
+          >
+            초기화
+          </button>
+
+          <button
+            onClick={handleSubmit}
+            type="button"
+            className={`${styles.formButton} ${styles.submitButton}`}
+          >
+            제출
+          </button>
+        </div>
+      </form>
+    </>
+  )
 }
 
-export default Registerform
+export default RegisterForm
