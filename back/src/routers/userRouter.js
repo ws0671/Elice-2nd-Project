fc4891935e907a2fa0948dc8846454847f9e5a90
SongheeNoh <s0n9h2@gmail.com>
2022-04-22 23:01:12 +0900

    Style : 화살표 함수로 변경, Router 계층 불필요한 errormessage 부분 삭제

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -5,7 +5,7 @@ import { userAuthService } from "../services/userService"
 
 const userAuthRouter = Router()
 
-userAuthRouter.post("/user/register", async function (req, res, next) {
+userAuthRouter.post("/user/register", async (req, res, next) => {
   try {
     if (is.emptyObject(req.body)) {
       throw new Error(
@@ -23,17 +23,13 @@ userAuthRouter.post("/user/register", async function (req, res, next) {
       password,
     })
 
-    if (newUser.errorMessage) {
-      throw new Error(newUser.errorMessage)
-    }
-
     res.status(201).json(newUser)
   } catch (error) {
     next(error)
   }
 })
 
-userAuthRouter.post("/user/login", async function (req, res, next) {
+userAuthRouter.post("/user/login", async (req, res, next) => {
   try {
     // req (request) 에서 데이터 가져오기
     const { email, password } = req.body
@@ -41,63 +37,43 @@ userAuthRouter.post("/user/login", async function (req, res, next) {
     // 위 데이터를 이용하여 유저 db에서 유저 찾기
     const user = await userAuthService.getUser({ email, password })
 
-    if (user.errorMessage) {
-      throw new Error(user.errorMessage)
-    }
-
     res.status(200).send(user)
   } catch (error) {
     next(error)
   }
 })
 
-userAuthRouter.get(
-  "/user/current",
-  loginRequired,
-  async function (req, res, next) {
-    try {
-      // jwt토큰에서 추출된 사용자 id를 가지고 db에서 사용자 정보를 찾음.
-      const userId = req.currentUserId
-      const currentUserInfo = await userAuthService.getUserInfo({
-        userId,
-      })
+userAuthRouter.get("/user/current", loginRequired, async (req, res, next) => {
+  try {
+    // jwt토큰에서 추출된 사용자 id를 가지고 db에서 사용자 정보를 찾음.
+    const userId = req.currentUserId
+    const currentUserInfo = await userAuthService.getUserInfo({
+      userId,
+    })
 
-      if (currentUserInfo.errorMessage) {
-        throw new Error(currentUserInfo.errorMessage)
-      }
-
-      res.status(200).send(currentUserInfo)
-    } catch (error) {
-      next(error)
-    }
+    res.status(200).send(currentUserInfo)
+  } catch (error) {
+    next(error)
   }
-)
+})
 
-userAuthRouter.put(
-  "/user/current",
-  loginRequired,
-  async function (req, res, next) {
-    try {
-      // URI로부터 사용자 id를 추출함.
-      const userId = req.currentUserId
-      // body data 로부터 업데이트할 사용자 정보를 추출함.
-      const { nickname, email, password, bookmarks } = req.body ?? null
+userAuthRouter.put("/user/current", loginRequired, async (req, res, next) => {
+  try {
+    // URI로부터 사용자 id를 추출함.
+    const userId = req.currentUserId
+    // body data 로부터 업데이트할 사용자 정보를 추출함.
+    const { nickname, email, password, bookmarks } = req.body ?? null
 
-      const toUpdate = { nickname, email, password, bookmarks }
+    const toUpdate = { nickname, email, password, bookmarks }
 
-      // 해당 사용자 아이디로 사용자 정보를 db에서 찾아 업데이트함. 업데이트 요소가 없을 시 생략함
-      const updatedUser = await userAuthService.setUser({ userId, toUpdate })
+    // 해당 사용자 아이디로 사용자 정보를 db에서 찾아 업데이트함. 업데이트 요소가 없을 시 생략함
+    const updatedUser = await userAuthService.setUser({ userId, toUpdate })
 
-      if (updatedUser.errorMessage) {
-        throw new Error(updatedUser.errorMessage)
-      }
-
-      res.status(200).json(updatedUser)
-    } catch (error) {
-      next(error)
-    }
+    res.status(200).json(updatedUser)
+  } catch (error) {
+    next(error)
   }
-)
+})
 
 userAuthRouter.delete("/:id", loginRequired, async (req, res, next) => {
   try {
@@ -113,7 +89,7 @@ userAuthRouter.delete("/:id", loginRequired, async (req, res, next) => {
 userAuthRouter.put(
   "/user/bookmark/:gameId",
   loginRequired,
-  async function (req, res, next) {
+  async (req, res, next) => {
     try {
       const userId = req.currentUserId
       const gameId = req.params.gameId
@@ -128,7 +104,7 @@ userAuthRouter.put(
 )
 
 // jwt 토큰 기능 확인용, 삭제해도 되는 라우터임.
-userAuthRouter.get("/afterlogin", loginRequired, function (req, res, next) {
+userAuthRouter.get("/afterlogin", loginRequired, (req, res, next) => {
   res
     .status(200)
     .send(
