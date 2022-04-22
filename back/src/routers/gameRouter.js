31006c510186eaebdcd9b8e51ee3d4ec4b7b5fb5
SHIN DOHEE <DOHEE@DESKTOP-VHAC2D0.localdomain>
2022-04-22 14:35:10 +0900

    [Feat] 개별 게임 정보 조회 기능 router 부분 만듦

--- back/src/routers/gameRouter.js
+++ back/src/routers/gameRouter.js
@@ -1,2 +1,21 @@
 import { Router } from "express"
 import { gameAuthService } from "../services/gameService"
+
+const gameAuthRouter = Router()
+
+gameAuthRouter.get("/game/:gameId", async function (req, res, next) {
+  try {
+    const gameId = req.params.gameId
+    const gameInfo = await gameAuthService.getGameInfo({
+      gameId,
+    })
+
+    if (gameInfo.errorMessage) {
+      throw new Error(gameInfo.errorMessage)
+    }
+
+    res.status(200).send(gameInfo)
+  } catch (error) {
+    next(error)
+  }
+})
