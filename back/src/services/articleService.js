4d3735a3c7eb1bf6b53636a0d686761d3ad18926
SHIN DOHEE <DOHEE@DESKTOP-VHAC2D0.localdomain>
2022-04-23 15:32:25 +0900

    [Feat] service층에 addArticle 함수 생성

--- back/src/services/articleService.js
+++ back/src/services/articleService.js
@@ -1,5 +1,16 @@
 import { Article } from "../db"
+import { v4 as uuidv4 } from "uuid"
 
-const articleAuthService = {}
+const articleAuthService = {
+  addArticle: async ({ author, category, title, content }) => {
+    const articleId = uuidv4()
+
+    const newArticle = { articleId, author, category, title, content }
+
+    const createdNewArticle = await Article.create({ newArticle })
+
+    return createdNewArticle
+  },
+}
 
 export { articleAuthService }
