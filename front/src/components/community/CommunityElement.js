70d5b4b77a0c71945e785e51b2c24375e87c3d40
joohe71 <joohe71@gmail.com>
2022-04-23 18:16:39 +0900

    Update : 제목 컬럼 클릭 시 경로 연결 및  스타일 수정

--- front/src/components/community/CommunityElement.js
+++ front/src/components/community/CommunityElement.js
@@ -1,14 +1,27 @@
+import { useNavigate } from "react-router-dom"
+import styled from "styled-components"
 const CommunityElement = ({ item }) => {
+  const navigate = useNavigate()
+  const clickHandler = () => {
+    navigate(`/community/${item.id}`)
+  }
   return (
-    <tr>
+    <Tr>
       <td>{item.id}</td>
-      <td>{item.title}</td>
+      <td onClick={clickHandler}>{item.title}</td>
       <td>관리자</td>
       <td>2022-04-23</td>
       <td>{item.id}</td>
       <td>{item.userId}</td>
-    </tr>
+    </Tr>
   )
 }
+
+const Tr = styled.tr`
+  td:nth-child(2):hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`
 
 export default CommunityElement
