09fa7639179e574fbdd4fd7b1a551f96e35a6826
SongheeNoh <s0n9h2@gmail.com>
2022-04-24 03:43:31 +0900

    Style : if문 errorMessage 제거

--- back/src/routers/gameRouter.js
+++ back/src/routers/gameRouter.js
@@ -1,6 +1,4 @@
 import { Router } from "express"
-import res from "express/lib/response"
-import { restart } from "nodemon"
 import { gameAuthService } from "../services/gameService"
 
 const gameAuthRouter = Router()
@@ -9,10 +7,6 @@ gameAuthRouter.get("/game/list/:page", async function (req, res, next) {
   try {
     const page = req.params.page
     const gamelist = await gameAuthService.getGames({ page })
-
-    if (gamelist.errorMessage) {
-      throw new Error(gamelist.errorMessage)
-    }
 
     res.status(200).send(gamelist)
   } catch (error) {
@@ -26,10 +20,6 @@ gameAuthRouter.get("/game/:gameId", async function (req, res, next) {
     const gameInfo = await gameAuthService.getGameInfo({
       gameId,
     })
-
-    if (gameInfo.errorMessage) {
-      throw new Error(gameInfo.errorMessage)
-    }
 
     res.status(200).send(gameInfo)
   } catch (error) {
