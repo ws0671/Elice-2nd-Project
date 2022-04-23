7df15960e3ad5d99c6b41083731704a2c0c7291c
SHIN DOHEE <DOHEE@DESKTOP-VHAC2D0.localdomain>
2022-04-23 11:43:07 +0900

    [Feat] Atricle Schema 완성

--- back/src/db/schemas/article.js
+++ back/src/db/schemas/article.js
@@ -1,8 +1,38 @@
-import {Schema, model} from mongoose
+import { Schema, model } from mongoose
 
 const ArticleSchema = new Schema(
     {
-
+        id: {
+            type : String,
+            required: true
+        },
+        author: {
+            type : String,
+            required: true,
+        },
+        category: {
+            type : String,
+            required: true,
+        },
+        title: {
+            type : String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        likes: {
+            type: [String],
+            default: []
+        },
+        tags: {
+            type: [String],
+            default: []
+        },
+    },
+    {
+        timestamps: true,
     }
 )
 
