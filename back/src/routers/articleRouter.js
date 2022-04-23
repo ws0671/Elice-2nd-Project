256aaa55e105732d1b0d4fb795e72d04f72f86f6
SHIN DOHEE <DOHEE@DESKTOP-VHAC2D0.localdomain>
2022-04-23 15:26:35 +0900

    [Feat] article 생성 라우터

--- back/src/routers/articleRouter.js
+++ back/src/routers/articleRouter.js
@@ -1,6 +1,26 @@
 import { Router } from "express"
+import { loginRequired } from "../middlewares/loginRequired"
 import { articleAuthService } from "../services/articleService"
 
 const articleAuthRouter = Router()
+
+articleAuthRouter.post(
+  "/article/create",
+  loginRequired,
+  async (req, res, next) => {
+    try {
+      const { author, category, title, content } = req.body
+
+      const newArticle = await articleAuthService.addArticle({
+        author,
+        category,
+        title,
+        content,
+      })
+
+      res.status(201).json(newArticle)
+    } catch (error) {}
+  }
+)
 
 export { articleAuthRouter }
