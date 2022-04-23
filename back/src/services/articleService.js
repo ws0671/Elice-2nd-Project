3a36e0ac4bda291c235840e9b2b1a367e8f5f7ee
SHIN DOHEE <DOHEE@DESKTOP-VHAC2D0.localdomain>
2022-04-23 16:04:05 +0900

    [Feat] service 층에 게시글 수정하는 함수 생성

--- back/src/services/articleService.js
+++ back/src/services/articleService.js
@@ -1,5 +1,6 @@
 import { Article } from "../db"
 import { v4 as uuidv4 } from "uuid"
+import { SetUtil } from "../common/setUtil"
 
 const articleAuthService = {
   addArticle: async ({ author, category, title, content }) => {
@@ -19,6 +20,19 @@ const articleAuthService = {
     }
 
     return article
+  },
+  setArticle: async ({ articleId, author, toUpdate }) => {
+    let article = await Article.findById({ articleId })
+
+    if (!article) {
+      throw new Error("존재하지 않는 게시물입니다.")
+    } else if (article.author !== author) {
+      throw new Error("수정권한이 없는 게시물입니다.")
+    }
+
+    const updateObject = SetUtil.compareValues(toUpdate, article)
+
+    article = await User.update({ userId, updateObject })
   },
 }
 
