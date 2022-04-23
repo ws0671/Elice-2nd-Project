4e02a3ef2ea667f3608f29be67d7c617230252ac
SHIN DOHEE <DOHEE@DESKTOP-VHAC2D0.localdomain>
2022-04-23 16:30:17 +0900

    [Fix] 새로운 게시글이 생성되지 않는 오류 해결

--- back/src/db/models/Article.js
+++ back/src/db/models/Article.js
@@ -2,7 +2,7 @@ import { ArticleModel } from "../schemas/article"
 
 const Article = {
   create: async ({ newArticle }) => {
-    const createdNewArticle = await ArticleModel.create({ newArticle })
+    const createdNewArticle = await ArticleModel.create(newArticle)
     return createdNewArticle
   },
   findById: async ({ articleId }) => {
