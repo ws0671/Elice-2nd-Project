3acc8f2cdf6d64228e296383e1714ec6d626778a
ws0671 <ws0671@naver.com>
2022-04-25 18:32:18 +0900

    Design: 게임 검색 레이아웃 완료

--- front/src/pages/GameSearch.js
+++ front/src/pages/GameSearch.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useState } from "react"
 import { useNavigate, useLocation } from "react-router-dom"
 import Nav from "react-bootstrap/Nav"
 import { UserStateContext, DispatchContext } from "../App"
@@ -15,16 +15,58 @@ function GameSearch() {
       <H1 className="mt-5">게임 검색</H1>
       <div className="searchBarContainer">
         <div className="searchBar">
-          <button>전체 목록</button>
-          <button>장르</button>
-          <button>플랫폼</button>
-          <button>이용등급</button>
+          <Button>전체 목록</Button>
+          <Button>장르</Button>
+          <Button>플랫폼</Button>
+          <Button>이용등급</Button>
         </div>
         <div>
-          <input />
-          <button>확인</button>
+          <input
+            style={{ width: "500px", height: "50px" }}
+            placeholder="타이틀로 검색"
+          />
+          <Button2> 확인</Button2>
         </div>
       </div>
+      <main>
+        <ImgDiv className="mt-4">
+          <div className="row1">
+            <img
+              style={{ width: "400px" }}
+              alt="게임 이미지"
+              src="/images/star.jpg"
+            />
+            <img
+              style={{ width: "400px" }}
+              alt="게임 이미지"
+              src="/images/star.jpg"
+            />
+            <img
+              style={{ width: "400px" }}
+              alt="게임 이미지"
+              src="/images/star.jpg"
+            />
+          </div>
+          <div className="row2">
+            <img
+              style={{ width: "400px" }}
+              alt="게임 이미지"
+              src="/images/star.jpg"
+            />
+            <img
+              style={{ width: "400px" }}
+              alt="게임 이미지"
+              src="/images/star.jpg"
+            />
+            <img
+              style={{ width: "400px" }}
+              alt="게임 이미지"
+              src="/images/star.jpg"
+            />
+          </div>
+        </ImgDiv>
+      </main>
+      <Footer>Footer 자리입니다.</Footer>
     </>
   )
 }
@@ -32,5 +74,30 @@ function GameSearch() {
 const H1 = styled.h1`
   text-align: center;
   margin-bottom: 100px;
+`
+const ImgDiv = styled.div`
+  div {
+    display: flex;
+    justify-content: center;
+  }
+  img {
+    margin: 10px;
+  }
+`
+const Button = styled.button`
+  padding: 10px 30px;
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 10px;
+`
+const Button2 = styled.button`
+  padding: 10px 30px;
+  border-radius: 5px;
+  border: 1px solid black;
+  background-color: #673ab7;
+`
+const Footer = styled.footer`
+  text-align: center;
+  height: 40vh;
 `
 export default GameSearch
