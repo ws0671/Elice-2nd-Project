9a9e0ded56143bdc7956720790094da214161a98
DOHEE <dohee212@naver.com>
2022-04-26 13:34:37 +0900

    [Fix] setUtil 함수에서 toUpdate와 model 변수명 수정

--- back/src/common/SetUtil.js
+++ back/src/common/SetUtil.js
@@ -1,14 +1,14 @@
 class SetUtil {
-  static compareValues(toUpdate, model) {
-    let updateObject = {}
+  static compareValues(updateData, originData) {
+    let updateObject = {};
 
-    Object.entries(toUpdate).forEach((element) => {
-      if (element[1] !== model[element[0]])
-        updateObject[element[0]] = element[1]
-    })
+    Object.entries(updateData).forEach((element) => {
+      if (element[1] !== originData[element[0]])
+        updateObject[element[0]] = element[1];
+    });
 
-    return updateObject
+    return updateObject;
   }
 }
 
-export { SetUtil }
+export { SetUtil };
