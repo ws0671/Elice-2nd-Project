8b5f8c96bb1ba6a6d8d9cbc37580330f756748ae
joohe71 <joohe71@gmail.com>
2022-04-26 18:59:04 +0900

    Update : 상세페이지 데이터 불러오기

--- front/src/components/community/CommunityDetail.js
+++ front/src/components/community/CommunityDetail.js
@@ -1,4 +1,14 @@
+import { useParams } from "react-router-dom"
+import { useEffect } from "react"
+import axios from "axios"
 const CommunityDetail = () => {
+  const params = useParams()
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+      .then((res) => console.log(res.data))
+  }, [])
+
   return <div>안녕</div>
 }
 
