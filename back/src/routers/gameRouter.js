16933a894e7899a10d2541aa924fd69906b32d1e
DOHEE <dohee212@naver.com>
2022-04-26 13:14:34 +0900

    [Fix] update 함수의 매개변수를 updateObject에서 toUpdate로 변경

--- back/src/routers/gameRouter.js
+++ back/src/routers/gameRouter.js
@@ -1,46 +1,46 @@
-import { Router } from "express"
-import { gameService } from "../services/gameService"
+import { Router } from "express";
+import { gameService } from "../services/gameService";
 
-const gameRouter = Router()
+const gameRouter = Router();
 
 gameRouter.get("/list/:page", async function (req, res, next) {
   try {
-    const page = req.params.page
-    const gamelist = await gameService.getGames({ page })
+    const page = req.params.page;
+    const gamelist = await gameService.getGames({ page });
 
-    res.status(200).send(gamelist)
+    res.status(200).send(gamelist);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 gameRouter.get("/:gameId", async function (req, res, next) {
   try {
-    const gameId = req.params.gameId
+    const gameId = req.params.gameId;
     const gameInfo = await gameService.getGameInfo({
       gameId,
-    })
+    });
 
-    res.status(200).send(gameInfo)
+    res.status(200).send(gameInfo);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-gameAuthRouter.get("/topTen/:col", async function (req, res, next) {
+gameRouter.get("/topTen/:col", async function (req, res, next) {
   try {
-    const col = req.params.col
+    const col = req.params.col;
     const topTen = await gameAuthService.getTopTen({
       col,
-    })
+    });
 
     if (topTen.errorMessage) {
-      throw new Error(topTen.errorMessage)
+      throw new Error(topTen.errorMessage);
     }
 
-    res.status(200).send(topTen)
+    res.status(200).send(topTen);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
-export { gameRouter }
+});
+export { gameRouter };
