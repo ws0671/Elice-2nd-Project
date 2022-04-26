902a8eed0d7315b715fd938ad9041ee675b3d7cd
joohe71 <joohe71@gmail.com>
2022-04-27 00:17:46 +0900

    Update & Design : 커뮤니티 상세페이지 전체적인 틀 구현 및 디자인

--- front/src/components/community/CommunityDetail.js
+++ front/src/components/community/CommunityDetail.js
@@ -1,15 +1,54 @@
 import { useParams } from "react-router-dom"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import styled from "styled-components"
 import axios from "axios"
 const CommunityDetail = () => {
+  const [detail, setDetail] = useState({})
   const params = useParams()
   useEffect(() => {
     axios
       .get(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
-      .then((res) => console.log(res.data))
+      .then((res) => setDetail(res.data))
   }, [])
 
-  return <div>안녕</div>
+  return (
+    <Container>
+      <div className="detail title">{detail.title}</div>
+      <div className="detail">
+        <div>작성자</div>
+        <div>작성 시간 / 조회 수</div>
+      </div>
+      <div className="detail body">{detail.body}</div>
+    </Container>
+  )
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+
+  .detail {
+    width: 60%;
+
+    &:first-of-type {
+      font-size: 25px;
+      font-weight: bold;
+    }
+
+    &:not(:nth-of-type(2)) {
+      margin: 10px;
+      padding: 10px;
+    }
+
+    &:nth-of-type(2) {
+      margin: 0 10px;
+      padding: 0 10px;
+      font-size: 12px;
+    }
+  }
+`
 
 export default CommunityDetail
