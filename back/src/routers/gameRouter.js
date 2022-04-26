ba741e42bcc8d852ee5bb66f32a3830f334c4006
DOHEE <dohee212@naver.com>
2022-04-26 14:08:18 +0900

    [Fix] 저장 누락

--- back/src/routers/gameRouter.js
+++ back/src/routers/gameRouter.js
@@ -5,12 +5,6 @@ const gameRouter = Router();
 
 gameRouter.get("/list/:page", async function (req, res, next) {
   try {
-<<<<<<< HEAD
-    const page = req.params.page;
-    const gamelist = await gameService.getGames({ page });
-
-    res.status(200).send(gamelist);
-=======
     const page = Number(req.params.page);
     const numOfPageSkip = req.query.page ? Number(req.query.page) : undefined;
     const numOfPageLimit = req.query.limit
@@ -22,7 +16,6 @@ gameRouter.get("/list/:page", async function (req, res, next) {
       numOfPageLimit,
     });
     res.status(200).send(gameList);
->>>>>>> b257dc06fcf631bd92edb569b3aaf28f627ae4e6
   } catch (error) {
     next(error);
   }
@@ -30,11 +23,7 @@ gameRouter.get("/list/:page", async function (req, res, next) {
 
 gameRouter.get("/:gameId", async function (req, res, next) {
   try {
-<<<<<<< HEAD
-    const gameId = req.params.gameId;
-=======
     const gameId = Number(req.params.gameId);
->>>>>>> b257dc06fcf631bd92edb569b3aaf28f627ae4e6
     const gameInfo = await gameService.getGameInfo({
       gameId,
     });
@@ -45,20 +34,6 @@ gameRouter.get("/:gameId", async function (req, res, next) {
   }
 });
 
-<<<<<<< HEAD
-gameRouter.get("/topTen/:col", async function (req, res, next) {
-  try {
-    const col = req.params.col;
-    const topTen = await gameAuthService.getTopTen({
-      col,
-    });
-
-    if (topTen.errorMessage) {
-      throw new Error(topTen.errorMessage);
-    }
-
-    res.status(200).send(topTen);
-=======
 gameRouter.get("/rankedList/:colName", async function (req, res, next) {
   try {
     const colName = req.params.colName;
@@ -73,7 +48,6 @@ gameRouter.get("/rankedList/:colName", async function (req, res, next) {
     }
 
     res.status(200).send(rankedList);
->>>>>>> b257dc06fcf631bd92edb569b3aaf28f627ae4e6
   } catch (error) {
     next(error);
   }
