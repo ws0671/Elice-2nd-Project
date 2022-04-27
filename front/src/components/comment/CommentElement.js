7ae3dad5bd6add3708bc8f6f21068008c1be5177
joohe71 <joohe71@gmail.com>
2022-04-28 00:20:34 +0900

    Feat: 댓글 편집 함수 연결 및 편집 기능 구현

--- front/src/components/comment/CommentElement.js
+++ front/src/components/comment/CommentElement.js
@@ -1,7 +1,9 @@
 import { useState, useRef, useEffect } from "react"
 import styled from "styled-components"
 
-const CommentElement = ({ item, removeHandler }) => {
+const CommentElement = ({ item, removeHandler, editHandler }) => {
+  const [edit, setEdit] = useState(false)
+  const [comment, setComment] = useState(item.comment)
   const [view, setView] = useState(false)
   const viewMore = useRef([])
 
@@ -21,43 +23,89 @@ const CommentElement = ({ item, removeHandler }) => {
 
   return (
     !item.isDeleted && (
-      <Div view={view} className="comment-area">
-        <div>
-          <div className="nickname">{item.writeNickname}</div>
-          <div className="comment">{item.comment}</div>
-        </div>
-        <img
-          ref={(el) => (viewMore.current[0] = el)}
-          src="/images/viewmore.png"
-          alt="더보기"
-          onClick={(e) => {
-            setView((prev) => !prev)
-            console.log(viewMore)
-          }}
-        ></img>
-        <ul className="dropdown">
-          <li ref={(el) => (viewMore.current[1] = el)}>수정</li>
-          <li
-            ref={(el) => (viewMore.current[2] = el)}
-            onClick={() => {
-              removeHandler(item)
-            }}
-          >
-            삭제
-          </li>
-        </ul>
+      <Div edit={edit} view={view} className="comment-area">
+        {edit ? (
+          <>
+            <textarea
+              className="edit"
+              onChange={(e) => {
+                setComment(e.target.value)
+                console.log(comment)
+              }}
+              value={comment}
+              placeholder="20자 이상 적어주세요."
+            ></textarea>
+            <ButtonGroup>
+              <button
+                onClick={() => {
+                  editHandler(item, comment)
+                  setEdit(false)
+                }}
+              >
+                확인
+              </button>
+              <button
+                onClick={() => {
+                  setComment(item.comment)
+                  setEdit(false)
+                }}
+              >
+                취소
+              </button>
+            </ButtonGroup>
+          </>
+        ) : (
+          <>
+            <div>
+              <div className="nickname">{item.writeNickname}</div>
+              <div className="comment">{item.comment}</div>
+            </div>
+
+            <img
+              ref={(el) => (viewMore.current[0] = el)}
+              src="/images/viewmore.png"
+              alt="더보기"
+              onClick={(e) => {
+                setView((prev) => !prev)
+                console.log(viewMore)
+              }}
+            ></img>
+            <ul className="dropdown">
+              <li
+                ref={(el) => (viewMore.current[1] = el)}
+                onClick={() => {
+                  setEdit(true)
+                }}
+              >
+                수정
+              </li>
+              <li
+                ref={(el) => (viewMore.current[2] = el)}
+                onClick={() => {
+                  removeHandler(item)
+                }}
+              >
+                삭제
+              </li>
+            </ul>
+          </>
+        )}
       </Div>
     )
   )
 }
 
 const Div = styled.div`
+  .edit {
+    width:100%;
+    height: 15vh;
+  }
 
   margin-bottom: 20px;
   font-size: 13px;
   padding-bottom: 10px;
   display: flex;
-  flex-direction: row;
+  flex-direction: ${({ edit }) => (edit ? "column" : "row")};
   justify-content: space-between;
   // align-items: center;
 
@@ -94,4 +142,25 @@ const Div = styled.div`
       cursor: pointer;
     }
   `
+
+const ButtonGroup = styled.div`
+  left: 84%;
+  position: relative;
+  margin: -40px 10px 0 0;
+  width: 15%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+
+  button {
+    border: none;
+    padding: 4px 8px;
+    color: white;
+    font-weight: 700;
+
+    border-radius: 3px;
+    cursor: pointer;
+    background: #6c63ff;
+  }
+`
 export default CommentElement
