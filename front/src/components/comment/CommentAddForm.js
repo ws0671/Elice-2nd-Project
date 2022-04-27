fa8b6703bb383d8ce6a109b89dfeb7a557289d57
joohe71 <joohe71@gmail.com>
2022-04-28 00:28:00 +0900

    Add: 댓글 추가시 에러 핸들링 조건 추가

--- front/src/components/comment/CommentAddForm.js
+++ front/src/components/comment/CommentAddForm.js
@@ -14,8 +14,10 @@ const CommentAddForm = ({ clickHandler }) => {
       />
       <Button
         onClick={() => {
-          clickHandler(comment)
-          setComment("")
+          if (comment.length >= 20) {
+            clickHandler(comment)
+            setComment("")
+          } else alert("20자 이상을 적어주세요.")
         }}
       >
         등록
