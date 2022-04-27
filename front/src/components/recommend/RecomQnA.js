34d7e581f06f9844aa7a96768c2ef18dae51a45a
Eunyeoul Lee <yiio08@naver.com>
2022-04-27 11:42:27 +0900

    Test: styled-component 배경 테스트

--- front/src/components/recommend/RecomQnA.js
+++ front/src/components/recommend/RecomQnA.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react"
+import React, { useEffect, useRef, useState, useContext } from "react"
 import { useNavigate } from "react-router-dom"
 import { Container, Col, Row, Form, Button, FormGroup } from "react-bootstrap"
 import styled from "styled-components"
@@ -7,10 +7,21 @@ import { createGlobalStyle } from 'styled-components'
 import qnaList from "./data_example"
 
 function RecomQuestion() {
-    const navigate = useNavigate()
 
     const [qIdx, setQIdx] = useState(0)
     const [question, setQuestion] = useState(qnaList[qIdx].q)
+    const progressRef = useRef(null);
+
+    const PrevQnA = () => {
+        setQIdx(qIdx - 1)
+        setQuestion(qnaList[qIdx - 1].q)
+
+        const cntIdx = qIdx + 2
+        const count = Object.keys(qnaList).length
+        console.log('cntIdx, count', cntIdx, count)
+
+
+    }
 
     const NextQnA = () => {
         setQIdx(qIdx + 1)
@@ -20,54 +31,74 @@ function RecomQuestion() {
         const count = Object.keys(qnaList).length
         console.log('cntIdx, count', cntIdx, count)
 
-        if (cntIdx > count) {
-            navigate('recommend/result')
-        }
+        useEffect(() => {
+            if (qIdx > Object.keys(qnaList).length) {
+
+            }
+        }, [qIdx])
 
     }
 
     const count = Object.keys(qnaList).length
 
+    const Progress = ({ done }) => {
+
+    }
+
 
 
     return (
         <>
-            <GlobalStyle />
-            <QnaBox>
-                <div className="qBox">
-                    <h1>{question}</h1>
-                </div>
-                <div className="aBox">
-                    <p>
-                        {qnaList[qIdx].a.map((item) => {
-                            return (
-                                <>
-                                    <Sbutton>{item.answer}</Sbutton><br />
-                                </>
-                            )
-                        })}
-                    </p>
-                </div>
 
-
-                <Button className="btn-primary" onClick={NextQnA}>Next</Button>
-            </QnaBox>
+            <GlobalStyle>
+                <QnaBox>
+                    <div className="qBox">
+                        <h1>{question}</h1>
+                    </div>
+                    <div className="aBox">
+                        <p>
+                            {qnaList[qIdx].a.map((item, idx) => {
+                                return (
+                                    <>
+                                        <Sbutton key={idx}>{item.answer}</Sbutton><br />
+                                    </>
+                                )
+                            })}
+                        </p>
+                    </div>
+                    <Button className="btn-primary" onClick={PrevQnA}>Prev</Button>
+                    <Button className="btn-primary" onClick={NextQnA}>Next</Button>
+                    <Status className="mx-auto mt-5">
+                        <StatusBar />
+                    </Status>
+                </QnaBox>
+            </GlobalStyle>
 
         </>
     )
 
 }
-const GlobalStyle = createGlobalStyle`
+
+const GlobalStyle = styled.div`
+    width: 100vh;
+    height: 100vh;
+    font-family: "Roboto", sans-serif;
+    background-color: red; 
+  
+`
+
+/* const GlobalStyle = createGlobalStyle`
   body {
      font-family: "Roboto", sans-serif;
      background-color: #f8fafb; 
   }
-`
-const QnaBox = styled.div`
+` */
+
+const QnaBox = styled.section`
     position: absolute;
     left: 50%;
     top: 50%;
-    width: 500px;
+    width: 1000px;
     text-align: center;
     transform: translate(-50%, -50%);
 
@@ -97,5 +128,19 @@ const Sbutton = styled.button`
 }
 }
 `
+
+const Status = styled.div`
+    height: 20px;
+    width: 80%;
+    background-color: grey;
+    border-radius: 20px;
+`
+
+const StatusBar = styled.div`
+    height: 100%;
+    background-color: red;
+    border-radius: 20px;
+`
+
 
 export default RecomQuestion
