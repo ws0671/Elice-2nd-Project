13444638e4ccec89a8c2899b4a9cc3d66be56763
Eunyeoul Lee <yiio08@naver.com>
2022-04-27 16:36:33 +0900

    Feat: 첫페이지, 마지막 페이지 버튼 숨기고 나타내기, 진행바 구현

--- front/src/components/recommend/RecomQnA.js
+++ front/src/components/recommend/RecomQnA.js
@@ -1,16 +1,29 @@
-import React, { useEffect, useRef, useState, useContext } from "react"
-import { useNavigate } from "react-router-dom"
+import React, { useEffect, useRef, useState } from "react"
+import ReactDOM from 'react-dom'
+import { useNavigate } from 'react-router-dom';
 import { Container, Col, Row, Form, Button, FormGroup } from "react-bootstrap"
 import styled from "styled-components"
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { createGlobalStyle } from 'styled-components'
-import qnaList from "./data_example"
+import { qnaList } from "./data_example"
+import { First } from "react-bootstrap/esm/PageItem";
 
 function RecomQuestion() {
 
-    const [qIdx, setQIdx] = useState(0)
-    const [question, setQuestion] = useState(qnaList[qIdx].q)
-    const progressRef = useRef(null);
+    const [qIdx, setQIdx] = useState(0) // 질문 인덱스
+    const [question, setQuestion] = useState(qnaList[qIdx].q) // 질문 내용
+    const [loading, setLoading] = useState(true)
+
+    const progressRef = useRef(null)
+    const statusRef = useRef(null)
+    const navigate = useNavigate()
+
+    const [firstP, setFirstP] = useState(false)
+    const [lastP, setLastP] = useState(false) // 첫페이지인지 확인
+    const endPoint = Object.keys(qnaList).length; // 총 슬라이드(총 질문 개수)
+
+    const [select, setSelect] = useState([]) // 선택사항 저장
+
+
 
     const PrevQnA = () => {
         setQIdx(qIdx - 1)
@@ -19,9 +32,25 @@ function RecomQuestion() {
         const cntIdx = qIdx + 2
         const count = Object.keys(qnaList).length
         console.log('cntIdx, count', cntIdx, count)
-
+        statusRef.current.style.width = (100 / endPoint) * (qIdx - 1) + '%';
 
     }
+
+    const addAnswer = (answerText) => {
+        var a = document.querySelector('.aBox')
+        var answer = React.createElement('button', { children: answerText })
+        answer.classList.add('answerList')
+        ReactDOM.render(answer, a)
+        answer.addEventListener('click', () => {
+            var children = document.querySelectorAll('.answerList')
+            for (let i = 0; i < children.length; i++) {
+                children[i].disabled = true
+                children[i].style.display = 'none'
+            }
+        }, false)
+    }
+
+
 
     const NextQnA = () => {
         setQIdx(qIdx + 1)
@@ -29,28 +58,38 @@ function RecomQuestion() {
 
         const cntIdx = qIdx + 2
         const count = Object.keys(qnaList).length
-        console.log('cntIdx, count', cntIdx, count)
 
-        useEffect(() => {
-            if (qIdx > Object.keys(qnaList).length) {
+        console.log('statusRef', statusRef)
+        statusRef.current.style.width = (100 / endPoint + 1) * (qIdx + 1) + '%';
 
-            }
-        }, [qIdx])
 
     }
 
-    const count = Object.keys(qnaList).length
 
-    const Progress = ({ done }) => {
+
+
+    /*console.log(qnaList[0].a[1].type)*/
+    const ChooseOne = () => {
+        qnaList[qIdx].a[qIdx].type.map((item) => console.log(item))
+
 
     }
 
+    //이전, 다음 버튼 감추기용
+    useEffect(() => {
+        qIdx == 0 ? setFirstP(true) : setFirstP(false)
+    }, [qIdx])
+
+    useEffect(() => {
+        qIdx == endPoint - 1 ? setLastP(true) : setLastP(false)
+    }, [qIdx])
 
 
     return (
         <>
 
-            <GlobalStyle>
+
+            <BodyStyle>
                 <QnaBox>
                     <div className="qBox">
                         <h1>{question}</h1>
@@ -66,33 +105,28 @@ function RecomQuestion() {
                             })}
                         </p>
                     </div>
-                    <Button className="btn-primary" onClick={PrevQnA}>Prev</Button>
-                    <Button className="btn-primary" onClick={NextQnA}>Next</Button>
+                    {firstP || (<Button className="btn-primary" onClick={PrevQnA}>Prev</Button>)}
+                    {lastP || <Button className="btn-primary" onClick={NextQnA}>Next</Button>}
+                    {lastP && <Button className="btn-primary" onClick={ChooseOne}>결과보기</Button>}
                     <Status className="mx-auto mt-5">
-                        <StatusBar />
+                        <StatusBar ref={statusRef} />
                     </Status>
                 </QnaBox>
-            </GlobalStyle>
+            </BodyStyle>
+
 
         </>
     )
 
 }
 
-const GlobalStyle = styled.div`
-    width: 100vh;
+const BodyStyle = styled.div`
+    width: 100vw;
     height: 100vh;
     font-family: "Roboto", sans-serif;
-    background-color: red; 
-  
+    background-color: #f8fafb;
 `
 
-/* const GlobalStyle = createGlobalStyle`
-  body {
-     font-family: "Roboto", sans-serif;
-     background-color: #f8fafb; 
-  }
-` */
 
 const QnaBox = styled.section`
     position: absolute;
--- front/src/components/recommend/RecomResult.js
+++ front/src/components/recommend/RecomResult.js
@@ -3,9 +3,57 @@ import { useNavigate } from "react-router-dom"
 import { Container, Col, Row, Form, Button, FormGroup } from "react-bootstrap"
 import styled from "styled-components"
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { createGlobalStyle } from 'styled-components'
+import { qnaList, infoList } from "./data_example"
+import select from './RecomQnA'
+
 
 function RecomResult() {
+    const endPoint = 12
+    const calResult = () => {
+        let pointArray = [
+            { name: 'mouse', value: 0, key: 0 },
+            { name: 'cow', value: 0, key: 1 },
+            { name: 'tiger', value: 0, key: 2 },
+            { name: 'rabbit', value: 0, key: 3 },
+            { name: 'dragon', value: 0, key: 4 },
+            { name: 'snake', value: 0, key: 5 },
+            { name: 'horse', value: 0, key: 6 },
+            { name: 'sheep', value: 0, key: 7 },
+            { name: 'monkey', value: 0, key: 8 },
+            { name: 'chick', value: 0, key: 9 },
+            { name: 'dog', value: 0, key: 10 },
+            { name: 'pig', value: 0, key: 11 },
+        ]
+
+        for (let i = 0; i < endPoint; i++) {
+            var target = qnaList[i].a[select[i]]
+            for (let j = 0; j < target.length; j++) {
+                for (let k = 0; k < pointArray.length; k++) {
+                    if (target.type[j] === pointArray[k].name) {
+                        pointArray[k].value += 1
+                    }
+                }
+
+            }
+
+        }
+
+        var resultArray = pointArray.sort(function (a, b) {
+            if (a.value > b.value) {
+                return -1
+            }
+            if (a.value < b.value) {
+                return 1
+            }
+            return 0
+        })
+
+        let resultWord = resultArray[0].key
+        return resultWord
+
+    }
+
+
     return (
         <h1>Result</h1>
     )
