7883b787b61e285ee9a15d7a2a6c50ee36edd893
DOHEE <dohee212@naver.com>
2022-04-27 16:12:48 +0900

    [Feat] addReview 함수(service) 생성

--- back/src/services/reviewService.js
+++ back/src/services/reviewService.js
@@ -2,6 +2,14 @@ import { Review } from "../db";
 import { v4 as uuidv4 } from "uuid";
 import { SetUtil } from "../common/setUtil";
 
-const ReviewService = {};
+const ReviewService = {
+  addReview: async ({ userId, gameId, content }) => {
+    const reviewId = uuidv4();
+    const newReview = { reviewId, userId, gameId, content };
+
+    const createdNewReview = await Review.create({ newReview });
+    return createdNewReview;
+  },
+};
 
 export { ReviewService };
