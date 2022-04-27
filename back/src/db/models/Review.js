7f34191642cc54f77f47ac6ad9549b84c6495ef1
DOHEE <dohee212@naver.com>
2022-04-27 17:02:55 +0900

    [Fix] 오타가 또 나옴

--- back/src/db/models/Review.js
+++ back/src/db/models/Review.js
@@ -2,7 +2,7 @@ import { ReviewModel } from "../schemas/review";
 
 const Review = {
   create: async ({ newReview }) => {
-    const createdNewReview = await ReviewModel.create({ newReview });
+    const createdNewReview = await ReviewModel.create(newReview);
     return createdNewReview;
   },
 
