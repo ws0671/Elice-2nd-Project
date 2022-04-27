6952ab8dd16eb935e51585e9c57b82982ad3588d
DOHEE <dohee212@naver.com>
2022-04-27 15:11:12 +0900

    [Feat] Comment 세미콜론

--- back/src/db/models/Comment.js
+++ back/src/db/models/Comment.js
@@ -1,28 +1,28 @@
-import { CommentModel } from "../schemas/comment"
+import { CommentModel } from "../schemas/comment";
 
 const Comment = {
   create: async ({ newComment }) => {
-    const createNewComment = await CommentModel.create(newComment)
-    return createNewComment
+    const createNewComment = await CommentModel.create(newComment);
+    return createNewComment;
   },
 
   findById: async ({ commentId }) => {
-    const comment = await CommentModel.findOne({ commentId })
-    return comment
+    const comment = await CommentModel.findOne({ commentId });
+    return comment;
   },
 
   update: async ({ commentId, toUpdate }) => {
-    const filter = { commentId }
-    const update = toUpdate
-    const option = { returnOriginal: false }
+    const filter = { commentId };
+    const update = toUpdate;
+    const option = { returnOriginal: false };
     const updateComment = await CommentModel.findOneAndUpdate(
       filter,
       update,
       option
-    )
+    );
 
-    return updateComment
+    return updateComment;
   },
-}
+};
 
-export { Comment }
+export { Comment };
