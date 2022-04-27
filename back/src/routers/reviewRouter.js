1eeb064c76a569269f4d1aa29f3ef571ea81a1ab
DOHEE <dohee212@naver.com>
2022-04-27 16:09:29 +0900

    [Feat] 리뷰 생성 라우터

--- back/src/routers/reviewRouter.js
+++ back/src/routers/reviewRouter.js
@@ -5,4 +5,21 @@ import { ReviewService } from "../services/reviewService";
 const ReviewRouter = Router();
 ReviewRouter.use(loginRequired);
 
+ReviewRouter.post("/", async (req, res, next) => {
+  try {
+    const { gameId, content } = req.body;
+    const userId = req.currentUserId;
+
+    const newReview = await ArticleService.addReview({
+      userId,
+      gameId,
+      content,
+    });
+
+    res.status(201).json(newReview);
+  } catch (error) {
+    next(error);
+  }
+});
+
 export { ReviewRouter };
