d7ec3c5e48d84b0b37d4b4356055f1b931b1b092
DOHEE <dohee212@naver.com>
2022-04-28 19:34:13 +0900

    [Feat] data 삽입

--- front/src/components/minigame/Roulette.js
+++ front/src/components/minigame/Roulette.js
@@ -1,6 +1,119 @@
 import React, { useState } from "react";
 import { Wheel } from "react-custom-roulette";
 
+const data = [
+  {
+    id: 1,
+    option: 100,
+    style: { backgroundColor: "#dbe4ff", textColor: "#dbe4ff" },
+  },
+  {
+    id: 2,
+    option: 100,
+    style: { backgroundColor: "#dbe4ff", textColor: "black" },
+  },
+  {
+    id: 3,
+    option: 100,
+    style: { backgroundColor: "#dbe4ff", textColor: "#dbe4ff" },
+  },
+  {
+    id: 4,
+    option: 200,
+    style: { backgroundColor: "#bac8ff", textColor: "#bac8ff" },
+  },
+  {
+    id: 5,
+    option: 200,
+    style: { backgroundColor: "#bac8ff", textColor: "black" },
+  },
+  {
+    id: 6,
+    option: 200,
+    style: { backgroundColor: "#bac8ff", textColor: "#bac8ff" },
+  },
+  {
+    id: 7,
+    option: 1000,
+    style: { backgroundColor: "#364fc7", textColor: "white" },
+  },
+  {
+    id: 8,
+    option: 100,
+    style: { backgroundColor: "#dbe4ff", textColor: "#dbe4ff" },
+  },
+  {
+    id: 9,
+    option: 100,
+    style: { backgroundColor: "#dbe4ff", textColor: "black" },
+  },
+  {
+    id: 10,
+    option: 100,
+    style: { backgroundColor: "#dbe4ff", textColor: "#dbe4ff" },
+  },
+  {
+    id: 11,
+    option: 300,
+    style: { backgroundColor: "#91a7ff", textColor: "#91a7ff" },
+  },
+  {
+    id: 12,
+    option: 300,
+    style: { backgroundColor: "#91a7ff", textColor: "white" },
+  },
+  {
+    id: 13,
+    option: 300,
+    style: { backgroundColor: "#91a7ff", textColor: "#91a7ff" },
+  },
+  {
+    id: 14,
+    option: 100,
+    style: { backgroundColor: "#dbe4ff", textColor: "#dbe4ff" },
+  },
+  {
+    id: 15,
+    option: 100,
+    style: { backgroundColor: "#dbe4ff", textColor: "black" },
+  },
+  {
+    id: 16,
+    option: 100,
+    style: { backgroundColor: "#dbe4ff", textColor: "#dbe4ff" },
+  },
+  {
+    id: 17,
+    option: 200,
+    style: { backgroundColor: "#bac8ff", textColor: "#bac8ff" },
+  },
+  {
+    id: 18,
+    option: 200,
+    style: { backgroundColor: "#bac8ff", textColor: "black" },
+  },
+  {
+    id: 19,
+    option: 200,
+    style: { backgroundColor: "#bac8ff", textColor: "#bac8ff" },
+  },
+  {
+    id: 20,
+    option: 500,
+    style: { backgroundColor: "#5c7cfa", textColor: "#5c7cfa" },
+  },
+  {
+    id: 21,
+    option: 500,
+    style: { backgroundColor: "#5c7cfa", textColor: "white" },
+  },
+  {
+    id: 22,
+    option: 500,
+    style: { backgroundColor: "#5c7cfa", textColor: "#5c7cfa" },
+  },
+];
+
 const Roulette = () => {
   const [spin, setSpin] = useState(false);
   const [pointIndex, setPointIndex] = useState();
