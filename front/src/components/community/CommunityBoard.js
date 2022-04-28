e24a6dc4b5c5954939895bb2fde4ed5a5071756d
joohe71 <joohe71@gmail.com>
2022-04-29 00:07:44 +0900

    Design: 페이지네이션 버튼 디자인 수정

--- front/src/components/community/CommunityBoard.js
+++ front/src/components/community/CommunityBoard.js
@@ -132,26 +132,27 @@ const Nav = styled.nav`
 const Button = styled.button`
   border: none;
   border-radius: 8px;
-  padding: 8px;
+  padding: 4px 8px;
   margin: 0;
-  background: black;
-  color: white;
+  // background: black;
+  // color: white;
   font-size: 1rem;
+  font-weight: bold;
 
   &:hover {
-    background: tomato;
+    background: rgba(108, 99, 255, 0.4);
     cursor: pointer;
     transform: translateY(-2px);
   }
 
   &[disabled] {
-    background: grey;
+    background: #cbc0d3;
     cursor: revert;
     transform: revert;
   }
 
   &[aria-current] {
-    background: deeppink;
+    background: rgba(108, 99, 255, 0.7);
     font-weight: bold;
     cursor: revert;
     transform: revert;
