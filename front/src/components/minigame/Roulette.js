3c86b73e6b8a81c6522537eda56c7c4d5cd6b4e3
DOHEE <dohee212@naver.com>
2022-04-28 19:06:35 +0900

    [Feat] 최종 포인트 값인 result useState 선언

--- front/src/components/minigame/Roulette.js
+++ front/src/components/minigame/Roulette.js
@@ -4,6 +4,7 @@ import { Wheel } from "react-custom-roulette";
 const Roulette = () => {
   const [spin, setSpin] = useState(false);
   const [pointIndex, setPointIndex] = useState();
+  const [result, setResult] = userState();
 };
 
 export default Roulette;
