88fe99777b020af22b3f455721419e1464657397
DOHEE <dohee212@naver.com>
2022-04-30 01:42:33 +0900

    [Feat] 예비용 라우터 주석처리

--- back/src/routers/likeRouter.js
+++ back/src/routers/likeRouter.js
@@ -27,6 +27,29 @@ LikeRouter.post("/", async (req, res, next) => {
   }
 });
 
+// LikeRouter.post("/:articleId", async (req, res, next) => {
+//   try {
+//     const userId = req.currentUserId; // 로그인 한 사용자
+//     const articleId = req.params.articleId;
+//     const author = req.body.author; // 게시글의 Id, 게시글 작성자의 userId
+
+//     if (userId == author) {
+//       // 로그인 사용자 = 게시글 작성자이면
+//       throw new Error("본인 글에는 좋아요 할 수 없습니다.");
+//     } else {
+//       // 본인 게시글이 아니면
+//       await likeService.addLike({
+//         userId,
+//         articleId,
+//       });
+
+//       res.status(204).end();
+//     }
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
 LikeRouter.delete("/", async (req, res, next) => {
   try {
     const userId = req.currentUserId; // 로그인 한 사용자
@@ -48,5 +71,28 @@ LikeRouter.delete("/", async (req, res, next) => {
     next(error);
   }
 });
+
+// LikeRouter.delete("/:articleId", async (req, res, next) => {
+//   try {
+//     const userId = req.currentUserId; // 로그인 한 사용자
+//     const articleId = req.params.articleId;
+//     const author = req.body.author; // 게시글의 Id, 게시글 작성자의 userId
+
+//     if (userId == author) {
+//       // 로그인 사용자 = 게시글 작성자이면
+//       throw new Error("본인 글에는 좋아요 할 수 없습니다.");
+//     } else {
+//       // 본인 게시글이 아니면
+//       await likeService.deleteLike({
+//         userId,
+//         articleId,
+//       });
+
+//       res.status(204).end();
+//     }
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 export { LikeRouter };
