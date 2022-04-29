02307e5ed2d838b19308f308b0a826a3951a9c97
DOHEE <dohee212@naver.com>
2022-04-30 01:22:42 +0900

    [Feat] likeService에 addLike 추가

--- back/src/services/likeService.js
+++ back/src/services/likeService.js
@@ -1,5 +1,23 @@
 import { Like, Article } from "../db";
 
-const likeService = {};
+const likeService = {
+  addLike: async ({ userId, articleId }) => {
+    const article = await Article.findById({ articleId });
+    if (!article) {
+      throw new Error("존재하지 않는 게시물입니다.");
+    }
+
+    const newLike = { userId, articleId };
+    const like = await Like.findByFilter(newLike);
+
+    if (like) {
+      throw new Error("이미 좋아요를 누른 게시물입니다.");
+    }
+
+    await Like.create({ newLike });
+    const toUpdate = { $inc: { like: 1 } };
+    await Article.update({ articleId, toUpdate });
+  },
+};
 
 export { likeService };
