6cd616a24331c779c9c431915dfb976820f33d81
DOHEE <dohee212@naver.com>
2022-04-30 02:55:29 +0900

    [Feat] MemorizeCards 컴포넌트의 기본 return값 설정

--- front/src/components/minigame/Cat Match/MemorizeCards.js
+++ front/src/components/minigame/Cat Match/MemorizeCards.js
@@ -1,7 +1,12 @@
 import "./MemorizeCards.css";
 
 const MemorizeCards = () => {
-  return;
+  return (
+    <div>
+      <h1>CAT MATCH</h1>
+      <button>NEW GAME</button>
+    </div>
+  );
 };
 
 export default MemorizeCards;
