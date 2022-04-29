5fa3d021865000cfc7a75bca0682191706cb7612
DOHEE <dohee212@naver.com>
2022-04-30 03:28:31 +0900

    [Feat] 뒷면을 클릭하면 실행될 함수를 handleClick으로 설정

--- front/src/components/minigame/Cat Match/SingleCard.js
+++ front/src/components/minigame/Cat Match/SingleCard.js
@@ -5,7 +5,12 @@ const SingleCard = ({ card }) => {
     <div className="card">
       <div>
         <img className="front" src={card.src} alt="card front" />
-        <img className="back" src="/img/뒷면.png" alt="card back" />
+        <img
+          className="back"
+          src="/img/뒷면.png"
+          onClick={handleClick}
+          alt="card back"
+        />
       </div>
     </div>
   );
