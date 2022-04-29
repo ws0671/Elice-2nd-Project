34350de6f60ac6160643725ec42689b07ccbf9b7
DOHEE <dohee212@naver.com>
2022-04-30 03:11:08 +0900

    [Feat] 카드를 섞고 cards 안에 넣어줌

--- front/src/components/minigame/Cat Match/MemorizeCards.js
+++ front/src/components/minigame/Cat Match/MemorizeCards.js
@@ -16,6 +16,8 @@ const MemorizeCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
   };
 
   return (
