703b470c49358fda386b5b54b7fbc745cf80ee55
DOHEE <dohee212@naver.com>
2022-04-30 01:26:22 +0900

    [Feat] likeRouter에 좋아요 취소하는 라우터(delete) 생성

--- back/src/routers/likeRouter.js
+++ back/src/routers/likeRouter.js
@@ -27,4 +27,26 @@ LikeRouter.post("/", async (req, res, next) => {
   }
 });
 
+LikeRouter.delete("/", async (req, res, next) => {
+  try {
+    const userId = req.currentUserId; // 로그인 한 사용자
+    const { articleId, author } = req.body; // 게시글의 Id, 게시글 작성자의 userId
+
+    if (userId == author) {
+      // 로그인 사용자 = 게시글 작성자이면
+      throw new Error("본인 글에는 좋아요 할 수 없습니다.");
+    } else {
+      // 본인 게시글이 아니면
+      await LikeService.deleteLike({
+        userId,
+        articleId,
+      });
+
+      res.status(204).end();
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 export { LikeRouter };
