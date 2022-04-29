73e11ea3dbd529e7ac136f3437678b83d322b9af
DOHEE <dohee212@naver.com>
2022-04-30 03:53:00 +0900

    [Feat] 클래스 이름을 flipped가 true면 flipped로 false면 설정하지 않게 만듦

--- front/src/components/minigame/Cat Match/SingleCard.js
+++ front/src/components/minigame/Cat Match/SingleCard.js
@@ -7,7 +7,7 @@ const SingleCard = ({ card, handleChoice, flipped }) => {
 
   return (
     <div className="card">
-      <div>
+      <div className={flipped ? "flipped" : ""}>
         <img className="front" src={card.src} alt="card front" />
         <img
           className="back"
