2a0113ab66e42fb6754c99d74f0c736b0b8f373c
DOHEE <dohee212@naver.com>
2022-04-30 01:15:04 +0900

    [Feat] likeRouter에 좋아요 누르는 라우터 (post) 생성

--- back/src/routers/likeRouter.js
+++ back/src/routers/likeRouter.js
@@ -5,4 +5,26 @@ import { LikeService } from "../services/reviewService";
 const LikeRouter = Router();
 LikeRouter.use(loginRequired);
 
+LikeRouter.post("/", async (req, res, next) => {
+  try {
+    const userId = req.currentUserId; // 로그인 한 사용자
+    const { articleId, author } = req.body; // 게시글의 Id, 게시글 작성자의 userId
+
+    if (userId == author) {
+      // 로그인 사용자 = 게시글 작성자이면
+      throw new Error("본인 글에는 좋아요 할 수 없습니다.");
+    } else {
+      // 본인 게시글이 아니면
+      await ArticleService.like({
+        userId,
+        articleId,
+      });
+
+      res.status(204).end();
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 export { LikeRouter };
