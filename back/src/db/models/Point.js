9ae9741e11e06909e3ee6cc83fcfdb110381b63e
DOHEE <dohee212@naver.com>
2022-05-01 03:42:39 +0900

    [Feat] point model 에 create 함수 추가

--- back/src/db/models/Point.js
+++ back/src/db/models/Point.js
@@ -1,6 +1,9 @@
 import { PointModel } from "../schemas/point";
 
 const Point = {
+  create: async ({ newPoint }) => {
+    await PointModel.create(newPoint);
+  },
   findByFilter: async (filter) => {
     const pointInfo = await PointModel.findOne(filter);
     return pointInfo;
