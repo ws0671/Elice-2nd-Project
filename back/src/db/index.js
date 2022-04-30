c86d61c3992c555af7b23dc4d0344867eeb90623
DOHEE <dohee212@naver.com>
2022-05-01 01:47:50 +0900

    [Feat] index.js 파일에 point 추가

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -10,6 +10,7 @@ import { Comment } from "./models/Comment";
 import { Like } from "./models/Like";
 import { ReviewSchema } from "./schemas/review";
 import { Review } from "./models/Review";
+import { Point } from "./models/Point";
 
 const DB_URL =
   process.env.MONGODB_URL ||
@@ -89,4 +90,5 @@ export {
   Comment,
   Like,
   Review,
+  Point,
 };
