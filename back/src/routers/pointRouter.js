94337bd8031dbc6d6ef5651ae7fbd5dd617575e7
DOHEE <dohee212@naver.com>
2022-05-01 01:40:28 +0900

    [Feat] 포인트 제도는 로그인 유저만 사용하도록 함

--- back/src/routers/pointRouter.js
+++ back/src/routers/pointRouter.js
@@ -2,5 +2,6 @@ import { Router } from "express";
 import { loginRequired } from "../middlewares/loginRequired";
 
 const PointRouter = Router();
+PointRouter.use(loginRequired);
 
 export { PointRouter };
