a76fd3d9964b10cbde809ac18bcd3731e4632434
DOHEE <dohee212@naver.com>
2022-05-01 03:13:18 +0900

    [Fix] 오타 수정

--- back/src/services/pointService.js
+++ back/src/services/pointService.js
@@ -3,8 +3,8 @@ import { SetUtil } from "../common/setUtil";
 
 const pointService = {
   checkPoint: async ({ userId, miniGame, year, month, day }) => {
-    // const date = `2022-04-29T15:00:00.000Z` //2014-12-24T00:00:00.000Z
-    const date = setUtil.calDate({ year, month, day });
+    // const date = `2022-04-29T15:00:00.000Z`
+    const date = SetUtil.calDate({ year, month, day });
 
     const filter = { userId, miniGame, createdAt: { $gte: date } };
 
