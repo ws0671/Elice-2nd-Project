a3afbe2aad156c0870ec89418b239d924a7d3952
DOHEE <dohee212@naver.com>
2022-05-01 03:38:55 +0900

    [Feat] point 추가하는 라우터 생성

--- back/src/routers/pointRouter.js
+++ back/src/routers/pointRouter.js
@@ -5,6 +5,20 @@ import { pointService } from "../services/pointService";
 const PointRouter = Router();
 PointRouter.use(loginRequired);
 
+PointRouter.post("/", async (req, res, next) => {
+  try {
+    const userId = req.currentUserId;
+    const { miniGame, point } = req.body;
+
+    const newPoint = { userId, miniGame, point };
+
+    await pointService.addPoint({ newPoint });
+    res.status(201).end();
+  } catch (error) {
+    next(error);
+  }
+});
+
 PointRouter.get("/:miniGame", async (req, res, next) => {
   try {
     const userId = req.currentUserId;
