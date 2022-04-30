ec48aeb390e90e2372cb59f99609d0a163c0f2ae
joohe71 <joohe71@gmail.com>
2022-04-30 15:06:04 +0900

    Update: 마이페이지 틀구현

--- front/src/pages/Mypage.js
+++ front/src/pages/Mypage.js
@@ -1,5 +1,26 @@
+import styled from "styled-components"
 const Mypage = () => {
-  return <div>마이페이지입니다.</div>
+  return (
+    <Main>
+      <div>
+        <div>OOO 님의 게임 성향은...</div>
+        <div>나의 게임 성향과 관심 게임 정보를 한 눈에 담아보세요</div>
+        <button>프로필 수정</button>
+      </div>
+      <div>
+        <div>OOO 님의 게임 성향은...</div>
+        <div>나의 게임 성향과 관심 게임 정보를 한 눈에 담아보세요</div>
+        <button>프로필 수정</button>
+      </div>
+    </Main>
+  )
 }
 
+const Main = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`
 export default Mypage
