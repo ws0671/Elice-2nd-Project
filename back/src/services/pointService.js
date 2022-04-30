5dfa7d84a7249e035cd3d5bab67afff7eedbd792
DOHEE <dohee212@naver.com>
2022-05-01 02:32:24 +0900

    [Feat] checkpoint 함수 정의

--- back/src/services/pointService.js
+++ back/src/services/pointService.js
@@ -1,5 +1,22 @@
 import { Point, User } from "../db";
 
-const pointService = {};
+const pointService = {
+  checkPoint: async ({ userId, miniGame, year, month, day }) => {
+    // const date = `2022-04-29T15:00:00.000Z` //2014-12-24T00:00:00.000Z
+    const date = setUtil.calDate({ year, month, day });
+
+    const filter = { userId, miniGame, createdAt: { $gte: date } };
+
+    let point = await Point.findByFilter(filter);
+
+    if (!point) {
+      point = { point: false };
+    } else {
+      point = { point: point.point };
+    }
+
+    return point;
+  },
+};
 
 export { pointService };
