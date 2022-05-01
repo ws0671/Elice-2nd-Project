a3ac85c00f0f6a42de3c74243fae47d6d832b725
Eunyeoul Lee <yiio08@naver.com>
2022-05-01 17:46:51 +0900

    Design: 첫번째 슬라이드 개수 조정

--- front/src/components/prologue/Carousel/FirstCarousel.js
+++ front/src/components/prologue/Carousel/FirstCarousel.js
@@ -14,7 +14,7 @@ function FirstCarousel() {
         centerPadding: "60px",
         autoplay: true,
         infinite: true,
-        slidesToShow: 4,
+        slidesToShow: 3,
         slidesToScroll: 1
     };
     return (
