f9961801333aca10bcb9fc601741880afeba923b
joohe71 <joohe71@gmail.com>
2022-05-01 14:18:01 +0900

    Refactor: 스타일 폴더 및 파일 생성해서 따로 관리

--- front/src/pages/Register.js
+++ front/src/pages/Register.js
@@ -1,5 +1,5 @@
 import RegisterForm from "../components/user/RegisterForm"
-import styled from "styled-components"
+import { Main, Container } from "../components/styles/RegisterStyle"
 
 const Register = () => {
   return (
@@ -13,35 +13,5 @@ const Register = () => {
     </Main>
   )
 }
-
-const Main = styled.div`
-  min-height: 100vh;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-around;
-`
-const Container = styled.div`
-  max-width: 100vh;
-  width: 60vh;
-
-  max-height: 100vh;
-  height: 80vh;
-
-  padding: 12px;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  &:not(:first-of-type) {
-    border: 2px solid #ced4da;
-    border-radius: 10px;
-  }
-
-  img {
-    width: 100vh;
-  }
-`
 
 export default Register
