482f50ce10be119bcdb0735cd52347b1dd0e8b45
joohe71 <joohe71@gmail.com>
2022-05-01 16:12:38 +0900

    Fix: class => className으로 수정

--- front/src/pages/LoginForm.js
+++ front/src/pages/LoginForm.js
@@ -1,19 +1,19 @@
-import React, { useState, useContext } from "react"
-import { useNavigate } from "react-router-dom"
-import { Container, Col, Row, Form, Button, FormGroup } from "react-bootstrap"
-import styled from "styled-components"
-import "bootstrap/dist/css/bootstrap.min.css"
-import { GiOysterPearl } from "react-icons/gi"
-import { createGlobalStyle } from "styled-components"
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { Container, Col, Row, Form, Button, FormGroup } from "react-bootstrap";
+import styled from "styled-components";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { GiOysterPearl } from "react-icons/gi";
+import { createGlobalStyle } from "styled-components";
 
-import * as Api from "../api"
-import { DispatchContext } from "../App"
+import * as Api from "../api";
+import { DispatchContext } from "../App";
 
 const GlobalStyle = createGlobalStyle`
   body {
   font-family: "Roboto", sans-serif;
   background-color: #f8fafb; }
-`
+`;
 
 const Sbutton = styled.button`
   outline: 0;
@@ -37,16 +37,16 @@ const Sbutton = styled.button`
       top: 1px;
     }
   }
-`
+`;
 
 function LoginForm() {
-  const navigate = useNavigate()
-  const dispatch = useContext(DispatchContext)
+  const navigate = useNavigate();
+  const dispatch = useContext(DispatchContext);
 
   //useState로 email 상태를 생성함.
-  const [email, setEmail] = useState("")
+  const [email, setEmail] = useState("");
   //useState로 password 상태를 생성함.
-  const [password, setPassword] = useState("")
+  const [password, setPassword] = useState("");
 
   //이메일이 abc@example.com 형태인지 regex를 이용해 확인함.
   const validateEmail = (email) => {
@@ -54,48 +54,48 @@ function LoginForm() {
       .toLowerCase()
       .match(
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      )
-  }
+      );
+  };
 
   //위 validateEmail 함수를 통해 이메일 형태 적합 여부를 확인함.
-  const isEmailValid = validateEmail(email)
+  const isEmailValid = validateEmail(email);
   // 비밀번호가 4글자 이상인지 여부를 확인함.
-  const isPasswordValid = password.length >= 4
+  const isPasswordValid = password.length >= 4;
   //
   // 이메일과 비밀번호 조건이 동시에 만족되는지 확인함.
-  const isFormValid = isEmailValid && isPasswordValid
+  const isFormValid = isEmailValid && isPasswordValid;
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
       const res = await Api.post("user/login", {
         email,
         password,
-      })
+      });
 
       // 유저 정보는 response의 data임.
-      const user = res.data
+      const user = res.data;
 
       // JWT 토큰은 유저 정보의 token임.
-      const jwtToken = user.token
+      const jwtToken = user.token;
       // sessionStorage에 "userToken"이라는 키로 JWT 토큰과 닉네임을 저장함.
 
-      sessionStorage.setItem("userToken", jwtToken)
-      sessionStorage.setItem("user", JSON.stringify(user))
+      sessionStorage.setItem("userToken", jwtToken);
+      sessionStorage.setItem("user", JSON.stringify(user));
 
       // dispatch 함수를 이용해 로그인 성공 상태로 만듦.
       dispatch({
         type: "LOGIN_SUCCESS",
         payload: user,
-      })
+      });
 
       // 기본 페이지로 이동함.
-      navigate("/", { replace: true })
+      navigate("/", { replace: true });
     } catch (err) {
-      console.log("로그인에 실패하였습니다.\n", err)
+      console.log("로그인에 실패하였습니다.\n", err);
     }
-  }
+  };
 
   return (
     <Container className="container">
@@ -122,7 +122,7 @@ function LoginForm() {
               style={{ marginBottom: "1rem" }}
             />
             <h3>Sign In</h3>
-            <p class="mb-4">' Game Pearl ' 에 오신 것을 환영합니다!</p>
+            <p className="mb-4">' Game Pearl ' 에 오신 것을 환영합니다!</p>
             <Form
               className="d-grid gap-2 col-10 mx-auto"
               onSubmit={handleSubmit}
@@ -189,7 +189,7 @@ function LoginForm() {
         </Row>
       </Row>
     </Container>
-  )
+  );
 }
 
-export default LoginForm
+export default LoginForm;
