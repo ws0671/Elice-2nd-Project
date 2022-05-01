a5faeb3ec3b2d1870ec4efec03ddf765e298cc5e
Eunyeoul Lee <yiio08@naver.com>
2022-05-02 00:47:26 +0900

    Test: recharts 테스트

--- front/src/pages/Prologue.js
+++ front/src/pages/Prologue.js
@@ -1,12 +1,11 @@
 import React from "react"
-import { useNavigate } from "react-router-dom"
-import styled from "styled-components"
 import PrologueMain from "../components/prologue/MainBanner/PrologueMain"
 import PrologueStyle from "../components/prologue/PrologueStyle"
 import { Content } from "../components/prologue/Contents/Contents"
 import { contentOne, contentTwo, contentThree } from "../components/prologue/Contents/ContentsData"
 import FirstContent from "../components/prologue/Contents/FirstContent.js"
 import LastContent from "../components/prologue/Contents/LastContent"
+import FirstChart from "../components/prologue/Charts/FristChart"
 
 
 function Prologue() {
@@ -20,6 +19,7 @@ function Prologue() {
             <Content {...contentTwo} />
             <Content {...contentThree} />
             <LastContent />
+            <FirstChart />
         </PrologueStyle>
     )
 }
