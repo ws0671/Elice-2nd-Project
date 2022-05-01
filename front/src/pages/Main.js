e78db8f6ea435dfa557f6c555ae34542219d4caf
SongheeNoh <s0n9h2@gmail.com>
2022-05-01 16:34:49 +0900

    Feat : front에서 react-youtube 적용 예시

--- front/src/pages/Main.js
+++ front/src/pages/Main.js
@@ -1,10 +1,18 @@
-import React, { useContext } from "react"
-import { useNavigate, useLocation } from "react-router-dom"
-import Nav from "react-bootstrap/Nav"
-import { UserStateContext, DispatchContext } from "../App"
-import { Container, Card, Row, Col, Button } from "react-bootstrap"
-import styled from "styled-components"
+import React, { useContext } from "react";
+import YouTube from "react-youtube";
+import { useNavigate, useLocation } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import { UserStateContext, DispatchContext } from "../App";
+import { Container, Card, Row, Col, Button } from "react-bootstrap";
+import styled from "styled-components";
 function Main() {
+  const opts = {
+    width: "250",
+    height: "150",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
   return (
     <>
       <div className="video">
@@ -102,6 +110,49 @@ function Main() {
             </Row>
           </Col>
         </Row>
+        <br />
+        <br />
+        <Row className="mb-3">
+          <h3>유튜브 인기 동영상</h3>
+        </Row>
+        <Row>
+          <Col>
+            <Row className="justify-content-center">
+              <YouTube videoId="lzOZZCju3ZY" opts={opts} />
+              <div className="mt-3">
+                전 세계가 극찬한 2022년 출시 기대작 국산 오픈월드 MMORPG TOP 8
+              </div>
+            </Row>
+          </Col>
+          <Col>
+            <Row className="justify-content-center">
+              <YouTube videoId="4KQ-Y23tUaA" opts={opts} />
+              <div className="mt-3">
+                최고 스펙 ㄷㄷ 게이머의 로망 오락실?(feat. 바다 뷰) 이젠 게임
+                하러 부산으로...★ / 스브스뉴스
+              </div>
+            </Row>
+          </Col>
+          <Col>
+            <Row className="justify-content-center">
+              <YouTube videoId="pQ6agUdQq4U" opts={opts} />
+              <div className="mt-3">
+                &quot;베트남도 한국처럼 될 수 있죠?&quot; 베트남 학생이 갑자기
+                던진 돌발 질문에 30년동안 한국을 연구한 교수가 던진 말"
+              </div>
+            </Row>
+          </Col>
+          <Col>
+            <Row className="justify-content-center">
+              <YouTube videoId="1734COnhHbo" opts={opts} />
+              <div className="mt-3">
+                10년이 지나도 해외에서 극찬 받는 한국 액션 영화.
+                &#39;아저씨&#39; 최신 해외반응 &quot;최소 존윅, 그 이상&quot;
+                #아저씨 #원빈 #해외반응
+              </div>
+            </Row>
+          </Col>
+        </Row>
       </Container>
       <div style={{ width: "100%", height: "20vh" }}></div>
       <Div className="footer" style={{ backgroundColor: "#6C63FF" }}>
@@ -129,19 +180,19 @@ function Main() {
         </div>
       </Div>
     </>
-  )
+  );
 }
 
 const Image = styled.img`
   width: 200px;
   height: 200px;
   border-radius: 50%;
-`
+`;
 const NewsImg = styled.img`
   width: 250px;
   height: 150px;
   border-radius: 15%;
-`
+`;
 const Div = styled.div`
   text-align: center;
   div:nth-child(2) {
@@ -152,6 +203,6 @@ const Div = styled.div`
     display: flex;
     justify-content: space-evenly;
   }
-`
+`;
 
-export default Main
+export default Main;
