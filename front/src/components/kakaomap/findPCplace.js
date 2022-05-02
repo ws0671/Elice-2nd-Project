6b57f21ead40ff2d998c535c1fdc7d4cfe1a7625
SongheeNoh <s0n9h2@gmail.com>
2022-05-03 03:15:30 +0900

    Feat : 지도 타입 컨트롤과 줌 컨트롤 버튼 추가

--- front/src/components/kakaomap/findPCplace.js
+++ front/src/components/kakaomap/findPCplace.js
@@ -5,12 +5,23 @@ const FindPCplace = () => {
   useEffect(() => {
     var mapContainer = document.getElementById("map");
     var mapOptions = {
-      center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
       level: 3,
       mapTypeId: kakao.maps.MapTypeId.ROADMAP,
     };
 
     let map = new kakao.maps.Map(mapContainer, mapOptions); // 지도를 생성합니다.
+
+    // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+    var mapTypeControl = new kakao.maps.MapTypeControl();
+
+    // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+    // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+    // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+    var zoomControl = new kakao.maps.ZoomControl();
+    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
     // HTML5의 geolocaiton으로 사용할 수 있는지 확인합니다.
     if (navigator.geolocation) {
