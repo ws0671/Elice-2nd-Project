efb044304420be43196b2440d3eaaed953aaf882
joohe71 <joohe71@gmail.com>
2022-05-03 03:21:23 +0900

    Update: 마이페이지 수정중

--- front/src/pages/Mypage.js
+++ front/src/pages/Mypage.js
@@ -4,18 +4,24 @@ import * as Api from "../api";
 import { UserStateContext } from "../App";
 import MypageBarChart from "../components/mypage/MypageBarChart";
 import MypagePieChart from "../components/mypage/MypagePieChart";
+import MypageBookmark from "../components/mypage/MypageBookmark";
+import MypageReview from "../components/mypage/MypageReview";
+import MypageEditModal from "../components/mypage/MypageEditModal";
 
 const Mypage = () => {
   const [TagData, setTagData] = useState([]);
   const [total, setTotal] = useState(0);
   const userContext = useContext(UserStateContext);
   const [data, setData] = useState({});
+  const [gameData, setGameData] = useState([]);
+
   useEffect(() => {
     console.log(userContext.user.userId);
     Api.get("user", `${userContext.user.userId}/myPage`)
       .then((res) => {
         setData(res.data.user);
-        console.log(res.data);
+        console.log("마이페이지", res.data);
+        setGameData(res.data.bookmarks);
         setUserData(
           res.data.bookmarks.map((data) => ({
             name: data.name,
@@ -64,53 +70,66 @@ const Mypage = () => {
   };
 
   return (
-    <Main>
-      <div className="top">
-        <div>
-          <div className="title">
-            {data.nickname} 님의 <br />
-            게임 성향은...
+    <>
+      <Header></Header>
+      <Main>
+        <div className="top">
+          <div>
+            <div className="title">
+              {data.nickname} 님의 <br />
+              게임 성향은...
+            </div>
+            <div className="subtitle">
+              나의 게임 성향과 관심 게임 정보를
+              <br /> 한 눈에 담아보세요
+            </div>
+            <button
+              onClick={() =>
+                MypageEditModal(data.nickname, userContext.user.userId)
+              }
+            >
+              닉네임 수정
+            </button>
           </div>
-          <div className="subtitle">
-            나의 게임 성향과 관심 게임 정보를
-            <br /> 한 눈에 담아보세요
-          </div>
-          <button>프로필 수정</button>
-        </div>
-        <div className="chart">
-          {TagData && (
-            <MypagePieChart
-              total={total}
-              chartData={TagData.sort(function (a, b) {
-                return b.value - a.value;
+          <div className="chart">
+            {TagData && (
+              <MypagePieChart
+                total={total}
+                chartData={TagData.sort(function (a, b) {
+                  return b.value - a.value;
+                })}
+              />
+            )}
+            <MypageBarChart
+              chartData={userData.sort(function (a, b) {
+                return b.긍정적 - a.긍정적;
               })}
             />
-          )}
-          <MypageBarChart
-            chartData={userData.sort(function (a, b) {
-              return b.긍정적 - a.긍정적;
-            })}
-          />
+          </div>
         </div>
-      </div>
-      <div className="middle1">
-        <div className="topic">내가 찜한 게임</div>
-        <div>
-          <button>ALL</button>
-          <button>인기순</button>
-          <button>플레이타임순</button>
+        <div className="middle1">
+          <div className="topic common">내가 찜한 게임</div>
+          <div className="common">
+            <MypageBookmark gameData={gameData} />
+          </div>
         </div>
-      </div>
-      <div className="middle2">
-        <div className="topic">내가 쓴 리뷰</div>
-        <div></div>
-      </div>
-    </Main>
+        <div className="middle2">
+          <div className="topic common">내가 쓴 리뷰</div>
+          <MypageReview />
+          <MypageReview />
+          <MypageReview />
+        </div>
+      </Main>
+    </>
   );
 };
 
+const Header = styled.div`
+  height: 10vh;
+`;
 const Main = styled.div`
   min-height: 100vh;
+  padding: 10% 0;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -134,6 +153,57 @@ const Main = styled.div`
   .middle1 {
     text-align: basis;
     width: 80%;
+
+    button {
+      background: #f6f6f6;
+      font-weight: bold;
+      text-align: center;
+      border: 2px solid #e0e0e0;
+      color: #909090;
+      text-shadow: 0 1px 0 rgba(255, 255, 255, 0.75);
+      padding: 6px 18px;
+      margin: 0 5px -1px 0;
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+      text-decoration: none;
+      position: relative;
+      top: 1px;
+
+      z-index: 3;
+
+      &:hover {
+        background: white;
+        border-bottom: white 2px solid;
+        &[disabled] {
+          background: #cbc0d3;
+          cursor: revert;
+          transform: revert;
+        }
+
+        &[aria-current] {
+          background: rgba(108, 99, 255, 0.7);
+          font-weight: bold;
+          cursor: revert;
+          transform: revert;
+        }
+      }
+    }
+
+    .gameBookmarks {
+      height: 30vh;
+
+      display: flex;
+      flex-direction: row;
+      // justify-content: space-around;
+      border: 2px solid #e0e0e0;
+      padding: 10px 0;
+
+      img {
+        width: 90%;
+        height: 100%;
+        padding: 10px 0;
+      }
+    }
   }
 
   .middle2 {
@@ -142,10 +212,13 @@ const Main = styled.div`
   }
 
   .topic {
+    margin-top: 20px;
     font-size: 2em;
     font-weight: bold;
   }
-
+  .common {
+    padding: 20px 0;
+  }
   .chart {
     display: flex;
     flex-direction: row;
