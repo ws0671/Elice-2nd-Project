df9ac0c52b68889a1a5630d9f9c3b850fa27c34f
joohe71 <joohe71@gmail.com>
2022-05-02 12:59:37 +0900

    Feat: 마이페이지 BarChart 구현

---
+++ front/src/components/mypage/MypageBarChart.js
@@ -0,0 +1,56 @@
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+import { Custom } from "../styles/Mypage/MypageChart";
+
+const MypageBarChart = ({ chartData }) => {
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      return (
+        <Custom className="custom-tooltip">
+          <p className="label">{`${label}`}</p>
+          <p> {`긍정적 : ${payload[0].value} %`}</p>
+          <p> {`부정적 : ${payload[1].value} %`}</p>
+        </Custom>
+      );
+    }
+
+    return null;
+  };
+
+  return (
+    <>
+      <div style={{ textAlign: "center" }}>게임별 긍정/부정 리뷰</div>
+      <BarChart
+        width={500}
+        height={300}
+        data={chartData}
+        margin={{ top: 40, right: 40, bottom: 30, left: 40 }}
+      >
+        <XAxis style={{ fontWeight: "bold", display: "none" }} dataKey="name" />
+        <YAxis
+          style={{ fontWeight: "bold" }}
+          label={{
+            value: "(%)",
+            position: "top",
+            offset: 20,
+            fontWeight: "bold",
+          }}
+        />
+        <Tooltip content={<CustomTooltip />} />
+        <Legend />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <Bar dataKey="긍정적" stackId="a" barSize={30} fill="#36A2EB" />
+        <Bar dataKey="부정적" stackId="a" barSize={30} fill="#FF6384" />
+      </BarChart>
+    </>
+  );
+};
+
+export default MypageBarChart;
