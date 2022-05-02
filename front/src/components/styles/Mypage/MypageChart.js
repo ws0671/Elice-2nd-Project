7d40be75c42efe632882bb08eafc9e0937495be5
joohe71 <joohe71@gmail.com>
2022-05-02 14:16:30 +0900

    Add: 차트 스타일 파일 추가

---
+++ front/src/components/styles/Mypage/MypageChart.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Custom = styled.div`
+  background: rgba(255, 255, 255, 0.8);
+  padding: 10px;
+  border-radius: 5px;
+  .label {
+    font-weight: bold;
+  }
+`;
