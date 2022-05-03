1b0212bb61fb1f54a9a709bc597c6e39b6b7a624
DOHEE <dohee212@naver.com>
2022-05-03 11:03:51 +0900

    [Feat] import constants

--- front/src/components/minigame/Snake/SnakeBoard.js
+++ front/src/components/minigame/Snake/SnakeBoard.js
@@ -1,3 +1,11 @@
 import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import useInterval from "./useInterval";
+import {
+  CanvasSize,
+  SnakeStart,
+  AppleStart,
+  Scale,
+  Speed,
+  Directions,
+} from "./SnakeConstants";
