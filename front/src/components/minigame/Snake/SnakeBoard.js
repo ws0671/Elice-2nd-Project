12790e3552c004fbcb2f4208bb4804c2923b3d0f
DOHEE <dohee212@naver.com>
2022-05-03 18:24:29 +0900

    [Feat] useContext 추가

--- front/src/components/minigame/Snake/SnakeBoard.js
+++ front/src/components/minigame/Snake/SnakeBoard.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useContext } from "react";
 import useInterval from "./useInterval";
 import {
   CanvasSize,
