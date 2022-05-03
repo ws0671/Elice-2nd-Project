7c6e0669f6496c0631b6a48db8e348ebe8ac7dde
DOHEE <dohee212@naver.com>
2022-05-03 14:21:38 +0900

    [Feat] useEffect import

--- front/src/components/minigame/Cat Match/MemorizeCards.js
+++ front/src/components/minigame/Cat Match/MemorizeCards.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./MemorizeCards.css";
 import SingleCard from "./SingleCard";
 import axios from "axios";
