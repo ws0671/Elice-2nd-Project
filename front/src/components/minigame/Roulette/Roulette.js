318ecd22bcd2d1815034aa56cc545469b16054cb
DOHEE <dohee212@naver.com>
2022-05-03 14:11:31 +0900

    [Feat] useContext import

--- front/src/components/minigame/Roulette/Roulette.js
+++ front/src/components/minigame/Roulette/Roulette.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Wheel } from "react-custom-roulette";
 import data from "./rouletteData";
 
