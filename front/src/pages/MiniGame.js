0b9009322ecc5152acfe828b448e2599a9313097
SongheeNoh <s0n9h2@gmail.com>
2022-05-03 17:16:47 +0900

    Test : 게임 기록 불러오는 부분 수정중

--- front/src/pages/MiniGame.js
+++ front/src/pages/MiniGame.js
@@ -1,5 +1,4 @@
 import "./index.css";
-import axios from "axios";
 import React, { useState, useLayoutEffect, useEffect, useContext } from "react";
 import * as Api from "../api";
 import { UserStateContext } from "../App";
@@ -7,42 +6,41 @@ import Header from "../components/minigame/2048/component/Header";
 import AboveGame from "../components/minigame/2048/component/AboveGame";
 import Game from "../components/minigame/2048/component/Game";
 import useLocalStorageNumber from "../components/minigame/2048/hook/useLocalStorage";
-import { identity } from "lodash";
+import { identity, reject } from "lodash";
+
+const todayCheck = () => {
+  return new Promise((res, rej) => {
+    try {
+      const today = Api.get("point/2048");
+      res(today.data.point);
+    } catch {
+      rej(new Error("오늘의 게임 기록을 불러오지 못했습니다."));
+    }
+  });
+};
 
 const MiniGame = () => {
   // 로그인 유저 전역 데이터값
   const userContext = useContext(UserStateContext);
+  const today = todayCheck();
 
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useLocalStorageNumber("bestScore", 0);
-  const [getPoint, setGetPoint] = useState();
 
-  useLayoutEffect(() => {
-    // 오늘 2048 게임으로 포인트 얻었는지 여부 확인
-    const check = () => {
-      Api.get("point/2048")
-        .then((res) => res.data.point)
-        .then((res) => setGetPoint(res))
-        .then(console.log(getPoint));
-    };
-    check();
-  });
-
-  useEffect(() => {
-    console.log(getPoint);
-    if (getPoint) {
+  useEffect(async () => {
+    console.log(today);
+    if (today) {
       // 오늘 포인트 얻은 기록이 있으면
       alert(
         "오늘은 이미 100포인트를 얻으셨습니다. 게임은 무한으로 즐겨주세요 :)"
       );
     }
-  }, []);
+  });
 
   useEffect(() => {
-    if (!getPoint) {
+    if (!today) {
       // 오늘 얻은 포인트가 없으면
       if (score >= 50) {
-        setGetPoint(100);
         alert("축하합니다!! 100포인트를 얻으셨습니다.");
         const record = { miniGame: 2048, point: 100 };
         const point = { point: 100 };
