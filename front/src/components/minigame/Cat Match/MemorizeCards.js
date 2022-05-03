2cc13789ba8e47c23a31f56e891e8eb4343d45c2
DOHEE <dohee212@naver.com>
2022-05-03 14:36:23 +0900

    [Feat] 불필요한 axios 제거

--- front/src/components/minigame/Cat Match/MemorizeCards.js
+++ front/src/components/minigame/Cat Match/MemorizeCards.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import "./MemorizeCards.css";
 import SingleCard from "./SingleCard";
-import axios from "axios";
 
 const cardImages = [
   { src: "/img/고양이 1.jpg", matched: false },
