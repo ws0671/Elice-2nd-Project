c526ee44f02b848311772efda67b774b577bcc8e
SongheeNoh <s0n9h2@gmail.com>
2022-05-03 12:27:17 +0900

    Feat : 마커 폰트 검정색으로 변경

--- front/src/components/kakaomap/findPCplace.js
+++ front/src/components/kakaomap/findPCplace.js
@@ -207,7 +207,8 @@ const FindPCplace = () => {
     // 검색결과 목록 또는 마커를 클릭했을 때 호출되는 함수입니다
     // 인포윈도우에 장소명을 표시합니다
     function displayInfowindow(marker, title) {
-      var content = '<div style="padding:5px;z-index:1;">' + title + "</div>";
+      var content =
+        '<div style="padding:5px;z-index:1;color:black">' + title + "</div>";
 
       infowindow.setContent(content);
       infowindow.open(map, marker);
