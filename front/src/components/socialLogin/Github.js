5e8a47f17e5faa512b06ede3e1ebbbba3793e51d
DOHEE <dohee212@naver.com>
2022-05-05 02:24:21 +0900

    [Feat] 로딩 중 보여줄 이미지를 담은 Spinner 선언

--- front/src/components/socialLogin/Github.js
+++ front/src/components/socialLogin/Github.js
@@ -6,6 +6,19 @@ import axios from "axios";
 const Github = () => {
   const navigate = useNavigate();
   const dispatch = useContext(DispatchContext);
+
+  const Spinner = () => {
+    return (
+      <div className="text-center">
+        <img
+          src={`/img/고양이 1.jpg`}
+          alt="loading"
+          width="800px"
+          height="800px"
+        />
+      </div>
+    );
+  };
 };
 
 export default Github;
