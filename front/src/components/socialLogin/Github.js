a35048e402de67cc0efe87362146dad9ee8faaf6
DOHEE <dohee212@naver.com>
2022-05-05 02:20:40 +0900

    [Feat] Github component 기본 구조

--- front/src/components/socialLogin/Github.js
+++ front/src/components/socialLogin/Github.js
@@ -2,3 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { DispatchContext } from "../../App";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+
+const Github = () => {};
+
+export default Github;
