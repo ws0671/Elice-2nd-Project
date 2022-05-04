3031c0c36c343a2b71e07a2ddfb6e198fbb35398
DOHEE <dohee212@naver.com>
2022-05-04 19:51:59 +0900

    [Feat] github 로그인 버튼 눌렀을 때 들어올 로직?

--- back/src/routers/authRouter.js
+++ back/src/routers/authRouter.js
@@ -1,5 +1,23 @@
 import { Router } from "express";
+import { param } from "express/lib/request";
 
 const AuthRouter = Router();
+
+AuthRouter.get("/github", async (req, res, next) => {
+  try {
+    const baseUrl = "https://github.com/login/oauth/authorize";
+    const config = {
+      client_id: process.env.GITHUB_CLIENT,
+      scope: "read:user user:email",
+      allow_signup: true,
+    };
+    const parmas = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${parmas}`;
+
+    res.redirect(finalUrl);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export { AuthRouter };
