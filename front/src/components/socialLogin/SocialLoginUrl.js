bb34faa8df9182dbfdcd595c8faf55445786332d
DOHEE <dohee212@naver.com>
2022-05-05 02:12:44 +0900

    [Feat] authorize url을 리턴해주는 githubUrl함수 작성

--- front/src/components/socialLogin/SocialLoginUrl.js
+++ front/src/components/socialLogin/SocialLoginUrl.js
@@ -1,3 +1,13 @@
-const githubUrl = () => {};
+const githubUrl = () => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: process.env.REACT_APP_GITHUB_CLIENT,
+    scope: "read:user user:email",
+    allow_signup: true,
+  };
+  const parmas = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${parmas}`;
+  return finalUrl;
+};
 
 export { githubUrl };
