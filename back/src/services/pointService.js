c1b57f13a118ebb3467f3d75d57e64bb53aa3dcc
DOHEE <dohee212@naver.com>
2022-05-04 15:37:44 +0900

    [Feat] checkPoint 함수에서 miniGame을 route로 변경

--- back/src/services/pointService.js
+++ back/src/services/pointService.js
@@ -6,12 +6,12 @@ const pointService = {
   addPoint: async ({ newPoint }) => {
     await Point.create({ newPoint });
   },
-  checkPoint: async ({ userId, miniGame }) => {
+  checkPoint: async ({ userId, route }) => {
     const date =
       moment().startOf("day").subtract(9, "hours").format(`YYYY-MM-DD`) +
       `T15:00:00.000Z`;
 
-    const filter = { userId, miniGame, createdAt: { $gte: date } };
+    const filter = { userId, route, createdAt: { $gte: date } };
 
     let point = await Point.findByFilter(filter);
     if (!point) {
