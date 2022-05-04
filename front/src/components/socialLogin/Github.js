6ef08c0cb865b6a6a00ff7dbb848d0a8556f0c84
DOHEE <dohee212@naver.com>
2022-05-05 02:27:16 +0900

    [Feat] 백에 요청해서 유저 정보를 가져와 토큰을 따로 빼줌

--- front/src/components/socialLogin/Github.js
+++ front/src/components/socialLogin/Github.js
@@ -19,6 +19,13 @@ const Github = () => {
       </div>
     );
   };
+
+  const githubLogin = async (code) => {
+      try {
+          const res = await axios.get(`http://localhost:5001/auth/github?code=${code}`)
+          const jwtToken = res.data.jwtToken
+      }
+  }
 };
 
 export default Github;
