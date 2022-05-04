4f22cc4df220fbbad3f00741adb7397b28ffdece
DOHEE <dohee212@naver.com>
2022-05-04 14:06:36 +0900

    [Feat] userService에 Article import

--- back/src/services/userService.js
+++ back/src/services/userService.js
@@ -1,4 +1,4 @@
-import { User, Game, Review, Like } from "../db"; // from을 폴더(db) 로 설정 시, 디폴트로 index.js 로부터 import함.
+import { User, Game, Review, Like, Article } from "../db"; // from을 폴더(db) 로 설정 시, 디폴트로 index.js 로부터 import함.
 import bcrypt from "bcrypt";
 import { v4 as uuidv4 } from "uuid";
 import jwt from "jsonwebtoken";
