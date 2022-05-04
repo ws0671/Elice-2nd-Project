ca878057c44a5d5709e2356c8ad30c5d01c67217
DOHEE <dohee212@naver.com>
2022-05-05 02:35:21 +0900

    [Feat] Github import

--- front/src/App.js
+++ front/src/App.js
@@ -4,21 +4,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import * as Api from "./api";
 import { loginReducer } from "./reducer";
 
-import Header from "./components/Header"
-import Main from "./pages/Main"
-import GameSearch from "./pages/GameSearch"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Community from "./pages/Community"
-import CommunityDetail from "./components/community/CommunityDetail"
+import Header from "./components/Header";
+import Main from "./pages/Main";
+import GameSearch from "./pages/GameSearch";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Community from "./pages/Community";
+import CommunityDetail from "./components/community/CommunityDetail";
 import Recommend from "./pages/Recommend";
 import RecomQnA from "./components/recommend/RecomQnA";
-import RecomResult from "./components/recommend/RecomResult"
-import Prologue from "./pages/Prologue"
+import RecomResult from "./components/recommend/RecomResult";
+import Prologue from "./pages/Prologue";
 import Mypage from "./pages/Mypage";
 import TopChart from "./pages/TopChart";
 import CommunityAddForm from "./components/community/CommunityAddForm";
 import GameDetail from "./pages/GameDetail";
+import Github from "./components/socialLogin/Github";
 
 export const UserStateContext = createContext(null);
 export const DispatchContext = createContext(null);
