ec2d0a7fe10e6b3fddadbb1d7b40104c07b5e4f9
DOHEE <dohee212@naver.com>
2022-05-05 01:43:37 +0900

    [Feat] 로그인된 사용자의 정보를 가져오게 함

--- back/src/routers/authRouter.js
+++ back/src/routers/authRouter.js
@@ -6,6 +6,9 @@ const AuthRouter = Router();
 AuthRouter.get("/github", async (req, res, next) => {
   try {
     const code = req.query.code;
+    const user = await GithubService.getToken({ code });
+
+    res.status(201).json(user);
   } catch (error) {
     next(error);
   }
