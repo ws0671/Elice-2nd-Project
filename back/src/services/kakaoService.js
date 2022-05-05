51d4195eb179adae708366b933dd43610b36c1ee
DOHEE <dohee212@naver.com>
2022-05-05 18:28:53 +0900

    [Feat] KakaoService 기본 구조

--- back/src/services/kakaoService.js
+++ back/src/services/kakaoService.js
@@ -2,3 +2,7 @@ import { User } from "../db";
 import axios from "axios";
 import jwt from "jsonwebtoken";
 import { SetUtil } from "../common/setUtil";
+
+class KakaoService {}
+
+export { KakaoService };
