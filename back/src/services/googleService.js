f986347f9fbbb9287a8b182a707d591ad14453e4
DOHEE <dohee212@naver.com>
2022-05-05 14:01:01 +0900

    [Feat] token 해독 함수 작성

--- back/src/services/googleService.js
+++ back/src/services/googleService.js
@@ -3,6 +3,15 @@ import axios from "axios";
 import jwt from "jsonwebtoken";
 
 class GoogleService {
+  static base64urlDecode = (str) => {
+    return Buffer.from(this.base64urlUnescape(str), "base64").toString();
+  };
+
+  static base64urlUnescape = (str) => {
+    str += Array(5 - (str.length % 4)).join("=");
+    return str.replace(/\-/g, "+").replace(/_/g, "/");
+  };
+
   static getToken = async () => {
     const uri = "https://oauth2.googleapis.com/token";
     const config = {
