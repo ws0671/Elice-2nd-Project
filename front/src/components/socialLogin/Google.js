5ce0a17e18a4d1c81a653e779e313e6cc878a98e
DOHEE <dohee212@naver.com>
2022-05-05 14:47:01 +0900

    [Feat] 로딩 중 나타날 Spinner 정의

--- front/src/components/socialLogin/Google.js
+++ front/src/components/socialLogin/Google.js
@@ -6,6 +6,19 @@ import axios from "axios";
 const Google = () => {
   const navigate = useNavigate();
   const dispatch = useContext(DispatchContext);
+
+  const Spinner = () => {
+    return (
+      <div className="text-center">
+        <img
+          src={`/img/고양이 2.jpg`}
+          alt="loading"
+          width="800px"
+          height="800px"
+        />
+      </div>
+    );
+  };
 };
 
 export default Google;
