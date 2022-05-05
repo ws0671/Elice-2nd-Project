9299e5d4b91447ec9b5a0e1cf788db66e7927146
DOHEE <dohee212@naver.com>
2022-05-05 14:25:31 +0900

    [Feat] SetUtil import

--- back/src/services/googleService.js
+++ back/src/services/googleService.js
@@ -1,6 +1,7 @@
 import { User } from "../db";
 import axios from "axios";
 import jwt from "jsonwebtoken";
+import { SetUtil } from "../common/setUtil";
 
 class GoogleService {
   static base64urlDecode = (str) => {
