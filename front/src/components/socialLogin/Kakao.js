4997b55cf60b32e3bcf62bc04cc283853fd8c16e
DOHEE <dohee212@naver.com>
2022-05-05 20:43:03 +0900

    [Feat] Spinner 정의

--- front/src/components/socialLogin/Kakao.js
+++ front/src/components/socialLogin/Kakao.js
@@ -6,6 +6,19 @@ import axios from "axios";
 const Kakao = () => {
   const navigate = useNavigate();
   const dispatch = useContext(DispatchContext);
+
+  const Spinner = () => {
+    return (
+      <div className="text-center">
+        <img
+          src={`/img/고양이 3.jpg`}
+          alt="loading"
+          width="800px"
+          height="800px"
+        />
+      </div>
+    );
+  };
 };
 
 export default Kakao;
