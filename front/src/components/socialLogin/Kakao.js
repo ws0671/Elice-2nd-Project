b89ae1420665a1b4c601c078925d480a7aed1255
DOHEE <dohee212@naver.com>
2022-05-05 20:48:01 +0900

    [Feat] 유저가 회원가입하면 swal로 로그인 유도

--- front/src/components/socialLogin/Kakao.js
+++ front/src/components/socialLogin/Kakao.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { DispatchContext } from "../../App";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 const Kakao = () => {
   const navigate = useNavigate();
@@ -26,6 +27,16 @@ const Kakao = () => {
         `http://localhost:5001/auth/kakao?code=${code}`
       );
       const user = res.data;
+      if (user.register) {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "회원가입되었습니다. 로그인해주세요 :)",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        navigate("/login", { replace: true });
+      }
     } catch (err) {
       alert("로그인 실패");
       navigate("/", { replace: true });
