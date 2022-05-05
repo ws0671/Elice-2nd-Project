f8889762390e46ca84d5cba195c28983e2d3559c
DOHEE <dohee212@naver.com>
2022-05-05 16:33:05 +0900

    [Feat] 포인트를 올리고 나서 알림창으로 포인트가 올라갔음을 고지해줌

--- front/src/components/socialLogin/Github.js
+++ front/src/components/socialLogin/Github.js
@@ -55,6 +55,13 @@ const Github = () => {
             route: "Login",
             point: 100,
           });
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: `축하합니다! 100포인트를 얻으셨습니다!!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
         }
 
         navigate("/", { replace: true });
