d20a8c292a754c2e63b7acace8633f433d470a5f
DOHEE <dohee212@naver.com>
2022-05-05 18:30:48 +0900

    [Feat] token 요청 경로 설정

--- back/src/services/kakaoService.js
+++ back/src/services/kakaoService.js
@@ -6,6 +6,14 @@ import { SetUtil } from "../common/setUtil";
 class KakaoService {
   static getToken = async ({ code }) => {
     const baseUrl = "https://kauth.kakao.com/oauth/token";
+    const body = {
+      client_id: process.env.KAKAO_CLIENT,
+      redirect_uri: "http://localhost:3000/auth/kakao/callback",
+      grant_type: "authorization_code",
+      code,
+    };
+    const params = new URLSearchParams(body);
+    const finalUrl = `${baseUrl}?${params}`;
   };
 }
 
