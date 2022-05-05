619ae90b5de4e7a9498606ff7da290e9bf50f749
DOHEE <dohee212@naver.com>
2022-05-05 14:44:35 +0900

    [Feat] Google 파일 기본

--- front/src/components/socialLogin/Google.js
+++ front/src/components/socialLogin/Google.js
@@ -2,3 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { DispatchContext } from "../../App";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+
+const Google = () => {};
+
+export default Google;
