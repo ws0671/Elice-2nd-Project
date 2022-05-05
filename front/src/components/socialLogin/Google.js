e30c09d67ba4efa7f644d5b72eb29e09611e2dfe
DOHEE <dohee212@naver.com>
2022-05-05 16:29:21 +0900

    [Feat] 포인트를 얻으면 알림창으로 고지해줌

--- front/src/components/socialLogin/Google.js
+++ front/src/components/socialLogin/Google.js
@@ -55,6 +55,13 @@ const Google = () => {
             route: "Loing",
             point: 100,
           });
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "축하합니다! 100포인트를 얻으셨습니다!",
+            showConfirmButton: false,
+            timer: 1500,
+          });
         }
 
         navigate("/", { replace: true });
