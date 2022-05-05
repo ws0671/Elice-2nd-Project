19023d39a4931c1652d69d7defc423dc32bcd89c
DOHEE <dohee212@naver.com>
2022-05-05 13:52:53 +0900

    [Feat] token 요청 url 만듦

--- back/src/services/googleService.js
+++ back/src/services/googleService.js
@@ -2,6 +2,19 @@ import { User } from "../db";
 import axios from "axios";
 import jwt from "jsonwebtoken";
 
-class GoogleService {}
+class GoogleService {
+  static getToken = async () => {
+    const uri = "https://oauth2.googleapis.com/token";
+    const config = {
+      code,
+      client_id: process.env.GOOGLE_CLIENT,
+      client_secret: process.env.GOOGLE_SECRET,
+      redirect_uri: "http://localhost:3000/auth/google/callback",
+    };
+    const params = new URLSearchParams(config);
+
+    const finalUrl = `${uri}?${params}&grant_type=authorization_code`;
+  };
+}
 
 export { GoogleService };
