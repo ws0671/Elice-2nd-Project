b49210796aa7d5d19ee51c15b212c3a4395f4e67
joohe71 <joohe71@gmail.com>
2022-05-07 22:31:48 +0900

    Add : 결과페이지 동적차트 구현

--- front/src/components/recommend/RecomResultDetail.js
+++ front/src/components/recommend/RecomResultDetail.js
@@ -1,55 +1,83 @@
-import React, { useState, useContext, useEffect } from "react"
-import { useNavigate, useLocation } from "react-router-dom"
-import styled from "styled-components"
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { useState, useContext, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import "bootstrap/dist/css/bootstrap.min.css";
 import * as Api from "../../api";
-import { UserStateContext } from "../../App"
-import { Wrapper, MainImg, RemainderWrapper, Remainders, ButtonWrapper, Button } from "./ResultDetailStyle"
+import { UserStateContext } from "../../App";
+import {
+  Wrapper,
+  MainImg,
+  RemainderWrapper,
+  Remainders,
+  ButtonWrapper,
+  Button,
+} from "./ResultDetailStyle";
 import { BodyStyle, QnaBox, AnswerButton } from "./RecomStyle";
 import RecomSlider from "./RecomSlider";
-import RecomChart from './RecomChart'
+import RecomChart from "./RecomChart";
 
 function RecomResultDetail() {
-    const location = useLocation()
-    const navigate = useNavigate()
-    const userContext = useContext(UserStateContext)
-    const userId = userContext.user.userId
+  const location = useLocation();
+  const navigate = useNavigate();
+  const userContext = useContext(UserStateContext);
+  const userId = userContext.user.userId;
+  const [category, setCategory] = useState([]);
 
+  const [currentIdx, setCurrentIdx] = useState(0);
+  const [gameItem, setGameItem] = useState([]);
 
-    const [currentIdx, setCurrentIdx] = useState(0)
-    const [gameItem, setGameItem] = useState([])
+  const getCurrentIdx = (currentIdx) => {
+    setCurrentIdx(currentIdx);
+  };
 
-    const getCurrentIdx = (currentIdx) => {
-        setCurrentIdx(currentIdx)
+  const refresh = () => {
+    Api.delete("gameRecommend", userId);
+    navigate("/recommend");
+  };
 
+  const making = (v) => {
+    let realTagData = [];
+    const result = {};
+    if (v) {
+      for (var i in v) {
+        for (var j in v[i]) {
+          realTagData.push(v[i][j]);
+        }
+      }
     }
+    realTagData.forEach((x) => {
+      result[x] = (result[x] || 0) + 1;
+    });
 
+    return Object.keys(result).map((name) => ({
+      name,
+      value: result[name],
+      fullMark: 5,
+    }));
+  };
 
-    const refresh = () => {
-        Api.delete("gameRecommend", userId)
-        navigate('/recommend')
-    }
+  useEffect(() => {
+    setGameItem(location.state.recomItem);
+    setCategory(
+      making(location.state.recomItem?.map((v) => v.categories))
+        .sort(function (a, b) {
+          return b.value - a.value;
+        })
+        .splice(0, 6)
+    );
+  }, []);
 
-    useEffect(() => {
-        setGameItem(location.state.recomItem)
-
-    }, [])
-
-
-
-    return (
-        <div>
-            <BodyStyle>
-                <RecomSlider gameItem={gameItem} getCurrentIdx={getCurrentIdx} />
-                <RecomChart gameItem={gameItem} />
-                <ButtonWrapper>
-                    <Button onClick={refresh}>처음부터 다시!</Button>
-                </ButtonWrapper>
-            </BodyStyle>
-        </div>
-    )
-
-
+  return (
+    <div>
+      <BodyStyle>
+        <RecomSlider gameItem={gameItem} getCurrentIdx={getCurrentIdx} />
+        <RecomChart category={category} />
+        <ButtonWrapper>
+          <Button onClick={refresh}>처음부터 다시!</Button>
+        </ButtonWrapper>
+      </BodyStyle>
+    </div>
+  );
 }
 
-export default RecomResultDetail
+export default RecomResultDetail;
