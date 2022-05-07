e0f84ab7ddf8eae7dde1b8390f4d56bdb383be2b
joohe71 <joohe71@gmail.com>
2022-05-08 05:28:55 +0900

    Design: 게임 상세페이지 디자인 수정

--- front/src/components/styles/GameDetailStyle.js
+++ front/src/components/styles/GameDetailStyle.js
@@ -19,6 +19,14 @@ export const FlexBody = styled.div`
   }
   .information {
     position: relative;
+    background: rgba(255, 255, 255, 0.5);
+    box-shadow: 0 8px 32px #6869d0;
+    backdrop-filter: blur(2.5px);
+    border-radius: 10px;
+    // color: #fff;
+    text-transform: uppercase;
+    letter-spacing: 0.4rem;
+    padding: 30px 20px;
   }
   .bookmark {
     cursor: pointer;
@@ -29,6 +37,8 @@ export const FlexBody = styled.div`
 export const ScreenShot = styled.div`
   display: flex;
   justify-content: center;
+  padding: 20px;
+
   img {
     width: 400px;
   }
@@ -37,7 +47,18 @@ export const Footer = styled.footer`
   height: 20vh;
 `;
 export const Main = styled.div`
-  padding: 0 200px;
+  padding: 80px 200px 20px 200px;
+  background: linear-gradient(
+    180deg,
+    rgba(6, 3, 17, 0.5) 10.29%,
+    rgba(8, 0, 33, 0.5) 24.48%,
+    rgba(55, 55, 165, 0.5) 39.46%,
+    rgba(114, 45, 155, 0.5) 53%,
+    rgba(179, 26, 123, 0.5) 67.58%,
+    rgba(198, 35, 104, 0.305) 80.6%,
+    rgba(198, 35, 104, 0) 99.35%
+  );
+  background-blend-mode: multiply;
   .screenShot {
     margin-left: 55px;
     font-weight: bold;
